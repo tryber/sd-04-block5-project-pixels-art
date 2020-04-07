b2a341baf00d8f1ea0477c389290142f74d9a96b
matheusysd <matheusyurisd@gmail.com>
2020-04-07 16:59:33 -0300

    Pixel board and color palette added

---
+++ script.js
@@ -0,0 +1,11 @@
+const blackPixel = document.getElementById('black');
+blackPixel.style.backgroundColor = 'black';
+
+const bluePixel = document.getElementById('blue');
+bluePixel.style.backgroundColor = 'blue';
+
+const indigoPixel = document.getElementById('indigo');
+indigoPixel.style.backgroundColor = 'indigo';
+
+const greenPixel = document.getElementById('green');
+greenPixel.style.backgroundColor = 'green';
