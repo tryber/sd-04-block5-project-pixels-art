b339256d5417c1aef35935e0e9b6bd362e4465a0
Eduardo Santos <eduardo.debest81@gmail.com>
2020-04-07 16:41:58 -0300

    base

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+let colors;
+let pixelBoard;
 window.onload = function(){
-  
+  colors = document.getElementsByClassName("color");
+  pixelBoard = document.getElementById("pixel-board");
+  this.addGrid(5 , 5);
+}
+
+function addGrid(xsize , ysize){
+  for (let y = 0; y < ysize; y++) {
+    for (let x = 0; x < xsize; x++) {
+      let pixel = document.createElement("div");
+      pixel.className = "pixel";
+      pixelBoard.appendChild(pixel);
+    }
+  }
 }
