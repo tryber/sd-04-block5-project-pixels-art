8a7c4fcdd7471b9b30a04c3ffe71e5b08325727f
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-04-07 14:30:52 -0300

    add 'color-palette'

---
+++ script.js
@@ -0,0 +1,11 @@
+window.onload = () => {
+
+    let arrayColors = ["black", "red", "green", "blue"];
+    let elementsColor = document.getElementsByClassName("color");
+
+    // paint the elements background
+    for (let i = 0; i < elementsColor.length; i += 1) {
+        elementsColor[i].style.backgroundColor = arrayColors[i]
+    }
+
+}
