878b8278cdabcc94235b41df9619320057aba24c
ederusena <eder.sena@live.com>
2020-04-07 18:23:41 -0300

    Pixel colorindo

--- script.js
+++ script.js
@@ -2,11 +2,17 @@
 window.onload = () => {
   // variables
   const corPixel = document.getElementsByClassName('selected')[0].id;
-  const palette = document.getElementById('color-palette');
+  const color_palette = document.getElementById('color-palette');
+  const pixel_board = document.getElementById('pixel-board');
 
-  palette.addEventListener('click', (event) => {
+  color_palette.addEventListener('click', (event) => {
     document.querySelector('.selected').classList.remove('selected');
     event.target.classList.add('selected');
+   
+  });
+
+  pixel_board.addEventListener('click', (event) => {
+      event.target.style.backgroundColor = document.querySelector('.selected').id;
   });
  
 };
