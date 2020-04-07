7af218ee9e1c5d28369377728a645204a2dcdd55
Leandro Marten <leandromarten1@gmail.com>
2020-04-07 18:06:57 -0300

    fixed bugs CC try 1

--- script.js
+++ script.js
@@ -1,40 +1,34 @@
-window.onload = function(){
+window.onload = function () {
 
-let selected = document.getElementsByClassName('selected')
 let color1 = document.querySelector('#color1');
 let color2 = document.querySelector('#color2');
+let color3 = document.querySelector('#color3');
+let color4 = document.querySelector('#color4');
 let boardPixel = document.querySelectorAll('.pixel');
 let thatColor = "black";
 
 
-  color1.addEventListener('click', function(event){
+  color1.addEventListener('click', function(){
     thatColor = color1.classList[1];
   });
 
-  color2.addEventListener('click', function(event){
+  color2.addEventListener('click', function(){
     thatColor = color2.classList[1];    
   });
 
-  color3.addEventListener('click', function(event){
+  color3.addEventListener('click', function(){
     thatColor = color3.classList[1];
   });
 
-  color4.addEventListener('click', function(event){
+  color4.addEventListener('click', function(){
     thatColor = color4.classList[1];
   });
-
     //pintar o board pixel
-    for(let i=0; i<boardPixel.length; i+=1){
+    for (let i=0; i<boardPixel.length; i+=1){
       let pixel = boardPixel[i];
       pixel.addEventListener('click', function(){
-        //pixel.style.backgroundColor = "red";
-        pixel.classList.add(thatColor);
+      //pixel.style.backgroundColor = "red";
+      pixel.classList.add(thatColor);
       });
-    };
- 
-
- 
+    }; 
 };
-
-
-
