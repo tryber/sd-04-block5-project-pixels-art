0c863c04bcfc9c2944b073d7c7778a9e930328a8
Filipe de Castro <filipeyoga@gmail.com>
2020-04-07 20:23:11 -0300

    commit to code review

--- script.js
+++ script.js
@@ -1,49 +1,49 @@
-const color1 = document.querySelector('.color1');
-const color2 = document.querySelector('.color2');
-const color3 = document.querySelector('.color3');
-const color4 = document.querySelector('.color4');
-
-console.log(color1.style.backgroundColor);
-
+const color1 = document.querySelector(".color1");
+const color2 = document.querySelector(".color2");
+const color3 = document.querySelector(".color3");
+const color4 = document.querySelector(".color4");
+const pixelBoard = document.getElementById("pixel-board");
 var atual = color1;
-let p11 = document.querySelector('.p11');
 
-function selected(div) { 
-  div.classList.add('selected');
-};    
+function selected(div) {
+  div.classList.add("selected");
+}
 selected(color1);
 
-const cor = document.querySelector('.selected');
+const cor = document.querySelector(".selected");
 
-color1.addEventListener ('click' , function(e) {
-  e.target.classList.add('selected');
+color1.addEventListener("click", function (e) {
+  e.target.classList.add("selected");
+  atual.classList.remove("selected");
   if (atual !== color1) {
-    atual.classList.remove('selected');
     atual = color1;
-  };
+  }
 });
-color2.addEventListener ('click' , function(e) {
-  e.target.classList.add('selected');
+color2.addEventListener("click", function (e) {
+  e.target.classList.add("selected");
+  atual.classList.remove("selected");
   if (atual !== color2) {
-    atual.classList.remove('selected');
     atual = color2;
-  };
+  }
 });
-color3.addEventListener ('click' , function(e) {
-  e.target.classList.add('selected');
+color3.addEventListener("click", function (e) {
+  e.target.classList.add("selected");
+  atual.classList.remove("selected");
   if (atual !== color3) {
-    atual.classList.remove('selected');
     atual = color3;
-  };
+  }
 });
-color4.addEventListener ('click' , function(e) {
-  e.target.classList.add('selected');
-   if (atual !== color4) {
-    atual.classList.remove('selected');
+color4.addEventListener("click", function (e) {
+  e.target.classList.add("selected");
+  atual.classList.remove("selected");
+  if (atual !== color4) {
     atual = color4;
-  };
+  }
 });
 
-p11.addEventListener('click', function(e) {
-  console.log(atual.style.backgroundColor);
+pixelBoard.addEventListener("click", function (event) {
+  const selectedColor = document.querySelector(".selected");
+  console.log(event.target);
+  console.log(selectedColor);
+  event.target.style.backgroundColor = selectedColor.style.backgroundColor;
 });
