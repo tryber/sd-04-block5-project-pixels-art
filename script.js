b69ef2c52866a72050419e4e97ea73dd95c0c040
orlandomessias <programadorlando@gmail.com>
2020-04-07 20:05:53 -0300

    solving code climate issues

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 const pixelsPallete = document.querySelectorAll('.color');
-let color = 'rgb(0, 0, 0)'
-let pixels = document.querySelectorAll('.pixel')
+let color = 'rgb(0, 0, 0)';
+const pixels = document.querySelectorAll('.pixel');
 
-function removeClass(){
-    for(let i=0; i<pixelsPallete.length; i++){ 
-        pixelsPallete[i].classList.remove('selected')
+function removeClass() {
+  for (let i = 0; i < pixelsPallete.length; i += 1) {
+    pixelsPallete[i].classList.remove('selected');
+  }
+}
+
+for (let i = 0; i < pixelsPallete.length; i += 1) {
+  pixelsPallete[i].addEventListener('click', () => {
+    if (!pixelsPallete[i].classList.contains('selected')) {
+      removeClass();
+      color = window.getComputedStyle(pixelsPallete[i], null).getPropertyValue('background-color');
+      pixelsPallete[i].classList.add('selected');
     }
+  });
 }
 
-for(let i = 0; i < pixelsPallete.length; i=i+1){ 
-    pixelsPallete[i].addEventListener('click', () => {
-        if(!pixelsPallete[i].classList.contains('selected')){
-            removeClass()
-            color = window.getComputedStyle(pixelsPallete[i], null).getPropertyValue('background-color')
-            pixelsPallete[i].classList.add('selected')
-        }
-    })
-}
-
-for(let i=0; i<pixels.length; i++){
-    pixels[i].addEventListener('click', () => {
-        pixels[i].style.backgroundColor = color
-    })
+for (let i = 0; i < pixels.length; i += 1) {
+  pixels[i].addEventListener('click', () => {
+    pixels[i].style.backgroundColor = color;
+  });
 }
