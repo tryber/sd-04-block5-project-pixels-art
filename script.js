14b23b37173ceee8bcc20c69a4404ace4eb3e045
ederusena <eder.sena@live.com>
2020-04-07 18:12:38 -0300

    Add e remove class

--- script.js
+++ script.js
@@ -2,5 +2,11 @@
 window.onload = () => {
   // variables
   const corPixel = document.getElementsByClassName('selected')[0].id;
+  const palette = document.getElementById('color-palette');
 
+  palette.addEventListener('click', (event) => {
+    document.querySelector('.selected').classList.remove('selected');
+    event.target.classList.add('selected');
+  });
+ 
 };
