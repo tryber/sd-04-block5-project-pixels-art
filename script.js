c75df80ce5f6630f3c9d807f29afdc4f384a7294
joaosoutto <joaosoutto92@gmail.com>
2020-04-07 19:26:42 -0300

    adicionando botão e consertando coisas

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-window.onload = function(){
-    let color1 = document.getElementsByClassName('color')[0]; color1.style.backgroundColor = 'black';
-    let color2 = document.getElementsByClassName('color')[1]; color2.style.backgroundColor = 'olive';
-    let color3 = document.getElementsByClassName('color')[2]; color3.style.backgroundColor = 'red';
-    let color4 = document.getElementsByClassName('color')[3]; color4.style.backgroundColor = 'navy';
-
-}
+window.onload = function () {
+  let color1 = document.getElementsByClassName('color')[0]; color1.style.backgroundColor = 'black';
+  let color2 = document.getElementsByClassName('color')[1]; color2.style.backgroundColor = 'olive';
+  let color3 = document.getElementsByClassName('color')[2]; color3.style.backgroundColor = 'red';
+  let color4 = document.getElementsByClassName('color')[3]; color4.style.backgroundColor = 'navy';
+};
