67d6b4115090d871344a70ee64eaf00f80d66fe5
Eduardo Santos <eduardo.debest81@gmail.com>
2020-04-07 17:15:31 -0300

    v0.2

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
 let colors;
 let pixelBoard;
-window.onload = function(){
+window.onload = function () {
   colors = document.getElementsByClassName("color");
+  colors[0].className += " selected";
+  randomizePallet();
   pixelBoard = document.getElementById("pixel-board");
-  this.addGrid(5 , 5);
+  addGrid(5, 5);
+
 }
 
-function addGrid(xsize , ysize){
+function randomizePallet() {
+  for (let i = 1; i < colors.length; i++) {
+    colors[i].style.backgroundColor = randomColor();
+  }
+}
+
+function randomColor() {
+  let letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+
+function addGrid(xsize, ysize) {
   for (let y = 0; y < ysize; y++) {
     for (let x = 0; x < xsize; x++) {
       let pixel = document.createElement("div");
@@ -14,4 +33,5 @@ function addGrid(xsize , ysize){
       pixelBoard.appendChild(pixel);
     }
   }
+  pixelBoard.style.width = ysize * 40 + "px";
 }
