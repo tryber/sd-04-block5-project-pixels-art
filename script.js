38b11cfc1ddcef36b0895c827cec5b5b2eae2c27
Filipe de Castro <filipeyoga@gmail.com>
2020-04-07 20:49:56 -0300

    testing bot

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const color2 = document.querySelector(".color2");
 const color3 = document.querySelector(".color3");
 const color4 = document.querySelector(".color4");
 const pixelBoard = document.getElementById("pixel-board");
+const palette = document.querySelector("#color-palette");
 var atual = color1;
 
 function selected(div) {
@@ -12,33 +13,9 @@ selected(color1);
 
 const cor = document.querySelector(".selected");
 
-color1.addEventListener("click", function (e) {
+palette.addEventListener("click", function (e) {
+  document.querySelector('.selected').classList.remove('selected');
   e.target.classList.add("selected");
-  atual.classList.remove("selected");
-  if (atual !== color1) {
-    atual = color1;
-  }
-});
-color2.addEventListener("click", function (e) {
-  e.target.classList.add("selected");
-  atual.classList.remove("selected");
-  if (atual !== color2) {
-    atual = color2;
-  }
-});
-color3.addEventListener("click", function (e) {
-  e.target.classList.add("selected");
-  atual.classList.remove("selected");
-  if (atual !== color3) {
-    atual = color3;
-  }
-});
-color4.addEventListener("click", function (e) {
-  e.target.classList.add("selected");
-  atual.classList.remove("selected");
-  if (atual !== color4) {
-    atual = color4;
-  }
 });
 
 pixelBoard.addEventListener("click", function (event) {
