e6e72b14f9862af67de0e5d949ae20736d68d5d1
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-04-07 16:11:21 -0300

    add the paint function

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 window.onload = () => {
   const arrayColors = ['black', 'red', 'green', 'blue'];
   const elementsColor = document.getElementsByClassName('color');
-  const palette = document.getElementById('color-palette')
+  const palette = document.getElementById('color-palette');
+  const pixelBoard = document.getElementById('pixel-board');
   // paint the elements background
   for (let i = 0; i < elementsColor.length; i += 1) {
     elementsColor[i].style.backgroundColor = arrayColors[i];
   }
-  
+  // remove and add class selected
   palette.addEventListener('click', (event) => {
     document.querySelector('.selected').classList.remove('selected');
     event.target.classList.add('selected');
-  })
-  
-}
+  });
+  // paint the pixel board with the color that have the selected class
+  pixelBoard.addEventListener('click', (event) => {
+    const selectedColor = document.querySelector('.selected');
+    event.target.style.backgroundColor = selectedColor.style.backgroundColor;
+  });
+};
