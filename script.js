1a9c132797210e0db1be78e4fd96e20ce169dc93
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-04-07 17:05:48 -0300

    change buttons for divs in color palette

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-window.onload = () => {
-  // random colors
-  let arrayColors = ['black'];
-  function randomColors() {
-    for (let i = 1; i < 4; i++) {
-      const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-      arrayColors[i] = '#' + randomColor;
-    }
+// random colors
+const arrayColors = ['black'];
+function randomColors() {
+  for (let i = 1; i < 4; i += 1) {
+    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    arrayColors[i] = '#' + randomColor;
   }
-  randomColors();
+}
+randomColors();
+window.onload = () => {
   // variables
   const elementsColor = document.getElementsByClassName('color');
   const palette = document.getElementById('color-palette');
   const pixelBoard = document.getElementById('pixel-board');
-  const clearBoard = document.getElementById('clear-board')
+  const clearBoard = document.getElementById('clear-board');
   // paint the elements background
   for (let i = 0; i < elementsColor.length; i += 1) {
     elementsColor[i].style.backgroundColor = arrayColors[i];
@@ -31,7 +31,7 @@ window.onload = () => {
   clearBoard.addEventListener('click', () => {
     const pixels = document.querySelectorAll('.pixel');
     for (let pixel of pixels) {
-      pixel.style.backgroundColor = 'white'
+      pixel.style.backgroundColor = 'white';
     }
-  })
+  });
 };
