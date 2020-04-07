c56577ef2022758e3339311be5a0df347a18e3de
jessica-tavares <jessicatavareseel@gmail.com>
2020-04-07 20:54:27 -0300

    tentando ler os pixels

--- script.js
+++ script.js
@@ -4,23 +4,37 @@ const purple = document.getElementsByClassName('color')[1];
 const pink = document.getElementsByClassName('color')[2];
 const yellow = document.getElementsByClassName('color')[3];
 const clearButton = document.getElementById('clear-board');
+const pixel = [];
+for (let i=0; i < 25; i+=1){
+  pixel[i] = document.getElementsByClassName('pixel')[i];
+}
 
 // Funçoes
-localStorage.setItem('color','black');
+localStorage.setItem('color', 'black');
 
-function changeColor(n) {
-  var color = n;
+function saveColor(n) {
+  let color = n;
   console.log(color);
-  sessionStorage.setItem('color', color);
+  localStorage.removeItem("color")
+  localStorage.setItem("color", color);
 }
 
-function clearBoard(){
-  sessionStorage.clear()
+function clearBoard() {
+  localStorage.clear();
 }
 
+function changeColor(j) {
+  pixel[j].style.backgoundColor = localStorage.getItem("color");
+}
+
+window.onload = function() {
 // Criando os event Listener
-black.addEventListener('click', changeColor('black'));
-purple.addEventListener('click', changeColor('blueviolet'));
-pink.addEventListener('click', changeColor('rgb(230, 47, 160)'));
-yellow.addEventListener('click', changeColor('yellow'));
+black.addEventListener('click', saveColor('black'));
+purple.addEventListener('click', saveColor('blueviolet'));
+pink.addEventListener('click', saveColor('rgb(230, 47, 160)'));
+yellow.addEventListener('click', saveColor('yellow'));
 clearButton.addEventListener('click', clearBoard());
+for (let j=0; j<25; j+=1){
+  pixel[j].addEventListener('click', changeColor(j));
+}
+}
