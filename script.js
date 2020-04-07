0dabd62f1d54f832f94ca92ff461189de3ef7a7e
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-04-07 16:48:04 -0300

    add random colors

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 window.onload = () => {
-  const arrayColors = ['black', 'red', 'green', 'blue'];
+  // random colors
+  let arrayColors = ['black'];
+  function randomColors() {
+    for (let i = 1; i < 4; i++) {
+      const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+      arrayColors[i] = '#' + randomColor;
+    }
+  }
+  randomColors();
+  // variables
   const elementsColor = document.getElementsByClassName('color');
   const palette = document.getElementById('color-palette');
   const pixelBoard = document.getElementById('pixel-board');
+  const clearBoard = document.getElementById('clear-board')
   // paint the elements background
   for (let i = 0; i < elementsColor.length; i += 1) {
     elementsColor[i].style.backgroundColor = arrayColors[i];
@@ -17,10 +27,11 @@ window.onload = () => {
     const selectedColor = document.querySelector('.selected');
     event.target.style.backgroundColor = selectedColor.style.backgroundColor;
   });
+  // clear the board
+  clearBoard.addEventListener('click', () => {
+    const pixels = document.querySelectorAll('.pixel');
+    for (let pixel of pixels) {
+      pixel.style.backgroundColor = 'white'
+    }
+  })
 };
-function clearBoard() {
-  const pixels = document.querySelectorAll(".pixel");
-  for (pixel of pixels) {
-    pixel.style.backgroundColor = "white"
-  }
-}
