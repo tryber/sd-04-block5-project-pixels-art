bd9acb907aae2cb328a2ae542eadb2ab8522358c
Rangel Paolo <paolorangel1996@outlook.com>
2020-04-07 20:29:39 -0300

    Configurado JavaScript para adicionar e remover class selected.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const paleta = document.querySelectorAll('.color'); // Manipula as cores da paleta.
 let pixel = document.querySelectorAll('.pixel'); // Manipula o quadro de pixels.
+let elemento = document.querySelector('.selected'); // Elemento com a cor selecionada.
 let selectedColor; // Cor selecionada.
 
 function carregaCores () { // Define as cores da paleta.
@@ -7,7 +8,7 @@ function carregaCores () { // Define as cores da paleta.
     switch (number) {
       case 0:
 				paleta[number].style.backgroundColor = 'black';
-				selectedColor = document.querySelector('.selected').style.backgroundColor; // Cor padrão selecionada (Preto).
+				selectedColor = elemento.style.backgroundColor; // Cor padrão selecionada (Preto).
         break;
       case 1:
         paleta[number].style.backgroundColor = 'red';
@@ -34,6 +35,8 @@ function adcionaEventPaleta () { // Adiciona evento click em cada cor da paleta.
 	for (let p = 0; p < paleta.length; p+=1) { // For que percorre todos os div .pixel. 
 		paleta[p].addEventListener('click', function (event) {
 			selectedColor = event.target.style.backgroundColor;
+			removeSelected ();
+			event.target.classList.add('selected');
 		});
 	}
 }
@@ -44,6 +47,10 @@ function adicionaEventPixel () {
 			event.target.style.backgroundColor = selectedColor;
 		});
 	}
+}
+
+function removeSelected () {
+	document.querySelector('.selected').classList.remove('selected');
 }
 
 window.onload = function () {
