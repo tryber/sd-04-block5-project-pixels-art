35074a1b2527556071e6e478ed2901c5567402b7
juniomelos <juniomelos@gmail.com>
2020-04-07 19:41:56 -0300

    quesito 3

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-window.onload = function(){
+let blackColor = document.getElementById("black");
+let purpleColor = document.getElementById("purple");
+let redColor = document.getElementById("red");
+let greenColor = document.getElementById("green");
+
+blackColor.style.background = "black";
+purpleColor.style.background = "purple";
+redColor.style.background = "red";
+greenColor.style.background = "green";
 
 
 
 
-
-
-
-}
