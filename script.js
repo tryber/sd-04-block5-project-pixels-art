fde0ec6845599fdd95c8a7dd09473248043badd9
Filipe de Castro <filipeyoga@gmail.com>
2020-04-07 17:39:56 -0300

    selected to black

---
+++ script.js
@@ -0,0 +1,9 @@
+const color1 = document.querySelector('#color1');
+const color2 = document.querySelector('#color2');
+const color3 = document.querySelector('#color3');
+const color4 = document.querySelector('#color4');
+
+function selected(div) { 
+  div.class = 'selected';
+};
+selected(color1);
