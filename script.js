cbe28b34ead457008d52f12fd143ab75f21a6d2e
orlandomessias <programadorlando@gmail.com>
2020-04-07 18:03:26 -0300

    add event listener click on pallete

--- script.js
+++ script.js
@@ -2,7 +2,15 @@ let pixelsPallete = document.querySelectorAll('.color')
 for(let i=0; i<pixelsPallete.length; i++){
     pixelsPallete[i].addEventListener('click', () => {
         if(!pixelsPallete[i].classList.contains('selected')){
-            
+            removeClass()
+            pixelsPallete[i].classList.add('selected')
+            console.log(pixelsPallete[i])
         }
     })
+}
+
+function removeClass(){
+    for(let i=0; i<pixelsPallete.length; i++){
+        pixelsPallete[i].classList.remove('selected')
+    }
 }
