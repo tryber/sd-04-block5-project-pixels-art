899b970a9697ed879d2a18d49b92d314ffc68a27
Caio Magalhaes <caio.lmf@gmail.com>
2020-04-07 18:44:14 -0300

    Center elements and animate color palette

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function createBoard(pixels) {
   pixelBoard.innerHTML = '';
   for (let i = 0; i < pixels; i += 1) {
     const pixelRow = document.createElement('div');
-    pixelRow.className = 'pixel-row flex';
+    pixelRow.className = 'pixel-row flex center';
     for (let ia = 0; ia < pixels; ia += 1) {
       const pixel = document.createElement('div');
       pixel.className = 'pixel';
