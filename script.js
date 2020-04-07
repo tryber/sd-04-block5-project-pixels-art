2b2f8a2f7cb508c505d26e5fd8ddb0a197e6a4ad
Ronan Fernandes Machado <ronanf.sk@gmail.com>
2020-04-07 18:05:31 -0300

    criado o grid de pixels donamicamente

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-let palletChilds = document.getElementsByClassName("color");
-let colors = ["black", "red", "green", "blue"];
-for (let i = 0; i < palletChilds.length; i += 1){
+const palletChilds = document.getElementsByClassName('color');
+const colors = ['black', 'red', 'green', 'blue'];
+for (let i = 0; i < palletChilds.length; i += 1) {
   palletChilds[i].style.backgroundColor = colors[i];
+}
+const pixelBoard = document.getElementById('pixel-board');
+for (let i = 0; i < 5; i += 1) {
+  for (let j = 0; j < 5; j += 1) {
+    const div = document.createElement('div');
+    div.className = 'pixel';
+    pixelBoard.appendChild(div);
+  }
 }
