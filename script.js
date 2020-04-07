e66e492bde46e788a95f2905d289d2f5f9ae0e35
orlandomessias <programadorlando@gmail.com>
2020-04-07 17:55:51 -0300

    verify if contain class selected

---
+++ script.js
@@ -0,0 +1,8 @@
+let pixelsPallete = document.querySelectorAll('.color')
+for(let i=0; i<pixelsPallete.length; i++){
+    pixelsPallete[i].addEventListener('click', () => {
+        if(!pixelsPallete[i].classList.contains('selected')){
+            
+        }
+    })
+}
