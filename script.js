8d4082ffa927f06a5dc628b5260eae028115935f
Leandro Marten <leandromarten1@gmail.com>
2020-04-07 18:33:59 -0300

    try requisito 82

--- script.js
+++ script.js
@@ -5,17 +5,34 @@ window.onload = function () {
   const color4 = document.querySelector('#color4');
   const boardPixel = document.querySelectorAll('.pixel');
   let thatColor = 'black';
+  let selected = "selected";
   color1.addEventListener ('click', function() {
     thatColor = color1.classList[1];
+    selected = color1.classList.add('selected');
+    selected = color2.classList.remove('selected');
+    selected = color3.classList.remove('selected');
+    selected = color4.classList.remove('selected');
   });
   color2.addEventListener ('click', function() {
     thatColor = color2.classList[1];
+    selected = color2.classList.add('selected');
+    selected = color1.classList.remove('selected');
+    selected = color3.classList.remove('selected');
+    selected = color4.classList.remove('selected');
   });
   color3.addEventListener ('click', function() {
     thatColor = color3.classList[1];
+    selected = color3.classList.add('selected');
+    selected = color2.classList.remove('selected');
+    selected = color1.classList.remove('selected');
+    selected = color4.classList.remove('selected');
   });
   color4.addEventListener ('click', function() {
     thatColor = color4.classList[1];
+    selected = color4.classList.add('selected');
+    selected = color2.classList.remove('selected');
+    selected = color3.classList.remove('selected');
+    selected = color1.classList.remove('selected');
   });
   //  pintar o board pixel
   for (let i = 0; i < boardPixel.length; i += 1) {
