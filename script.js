024f3615745d8c6d7e9092b812b144319c29527d
orlandomessias <programadorlando@gmail.com>
2020-04-07 20:50:56 -0300

    reset board colors button

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const pixelsPallete = document.querySelectorAll('.color');
 let color = 'rgb(0, 0, 0)';
 const pixels = document.querySelectorAll('.pixel');
+const resetBtn = document.getElementById('clear-board');
 
 function removeClass() {
   for (let i = 0; i < pixelsPallete.length; i += 1) {
@@ -23,3 +24,9 @@ for (let i = 0; i < pixels.length; i += 1) {
     pixels[i].style.backgroundColor = color;
   });
 }
+
+resetBtn.addEventListener('click', () => {
+  for (let i = 0; i < pixels.length; i+= 1) {
+    pixels[i].style.backgroundColor = 'white';
+  }
+});
