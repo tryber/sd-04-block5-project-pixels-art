f0d416dd97facfda360ba3ec2e9285df8775f148
orlandomessias <programadorlando@gmail.com>
2020-04-07 19:20:50 -0300

    add event click to the pixels in the board

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 const pixelsPallete = document.querySelectorAll('.color');
-for(let i = 0; i < pixelsPallete.length; i=i+1){ 
-    pixelsPallete[i].addEventListener('click', () => {
-        if(!pixelsPallete[i].classList.contains('selected')){
-            removeClass()
-            pixelsPallete[i].classList.add('selected')
-            console.log(pixelsPallete[i])
-        }
-    })
-}
+let color = 'rgb(0, 0, 0)'
+let pixels = document.querySelectorAll('.pixel')
 
 function removeClass(){
     for(let i=0; i<pixelsPallete.length; i++){ 
         pixelsPallete[i].classList.remove('selected')
     }
+}
+
+for(let i = 0; i < pixelsPallete.length; i=i+1){ 
+    pixelsPallete[i].addEventListener('click', () => {
+        if(!pixelsPallete[i].classList.contains('selected')){
+            removeClass()
+            color = window.getComputedStyle(pixelsPallete[i], null).getPropertyValue('background-color')
+            pixelsPallete[i].classList.add('selected')
+        }
+    })
+}
+
+for(let i=0; i<pixels.length; i++){
+    pixels[i].addEventListener('click', () => {
+        pixels[i].style.backgroundColor = color
+    })
 }
