55379cae7ff965a5040527abd5cdcf0242731e7e
Caio Magalhaes <caio.lmf@gmail.com>
2020-04-07 18:09:13 -0300

    Codeclimate

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+/* eslint-disable no-loop-func */
+/* eslint-disable no-param-reassign */
+/* eslint-disable require-jsdoc */
 let selectedColor = null;
 const colorPalette = document.querySelectorAll('.color');
 colorPalette.forEach((color) => {
@@ -5,50 +8,50 @@ colorPalette.forEach((color) => {
     color.classList.add('selected');
     selectedColor = color;
   } else {
-    const red = Math.round(Math.random()*255);
-    const green = Math.round(Math.random()*255);
-    const blue = Math.round(Math.random()*255);
+    const red = Math.round(Math.random() * 255);
+    const green = Math.round(Math.random() * 255);
+    const blue = Math.round(Math.random() * 255);
     color.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
   }
-  color.addEventListener('click', function(e) {
+  color.addEventListener('click', () => {
     if (selectedColor) {
-      selectedColor.classList.remove('selected'); 
+      selectedColor.classList.remove('selected');
     }
     color.classList.add('selected');
     selectedColor = color;
   });
 });
 
-createBoard(5);
 function createBoard(pixels) {
   const pixelBoard = document.querySelector('#pixel-board');
   pixelBoard.innerHTML = '';
-  for (let i = 0; i < pixels  ; i++) {
+  for (let i = 0; i < pixels; i += 1) {
     const pixelRow = document.createElement('div');
     pixelRow.className = 'pixel-row flex';
-    for (let i = 0; i < pixels; i++) {
+    for (let i = 0; i < pixels; i += 1) {
       const pixel = document.createElement('div');
       pixel.className = 'pixel';
-      pixel.addEventListener('click', function(e) {
+      pixel.addEventListener('click', () => {
         pixel.style.backgroundColor = getComputedStyle(selectedColor).backgroundColor;
       });
       pixelRow.appendChild(pixel);
-      
     }
     pixelBoard.appendChild(pixelRow);
   }
 }
+// Create initial  board;
+createBoard(5);
 
 const clearBtn = document.querySelector('#clear-board');
-clearBtn.addEventListener('click', function(e) {
-  pixels = document.querySelectorAll('.pixel');
-  pixels.forEach(pixel => {
+clearBtn.addEventListener('click', () => {
+  const pixels = document.querySelectorAll('.pixel');
+  pixels.forEach((pixel) => {
     pixel.style.backgroundColor = 'rgb(255, 255, 255)';
   });
 });
 
 const generateBtn = document.querySelector('#generate-board');
-generateBtn.addEventListener('click', function (e) {
+generateBtn.addEventListener('click', () => {
   const boardSize = document.querySelector('#board-size').value;
   if (boardSize > 50) {
     createBoard(50);
@@ -56,5 +59,5 @@ generateBtn.addEventListener('click', function (e) {
     createBoard(5);
   } else {
     createBoard(boardSize);
-  };
+  }
 });
