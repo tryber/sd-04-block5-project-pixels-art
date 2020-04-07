1946b66a7d0ef0637e47174daf400b6797258da8
matheusysd <matheusyurisd@gmail.com>
2020-04-07 19:52:33 -0300

    paintPixel function and colors events changed

--- script.js
+++ script.js
@@ -1,80 +1,43 @@
-const blackPixel = document.getElementById('black');
-blackPixel.style.backgroundColor = 'black';
+  const blackPixel = document.getElementById('black');
+  blackPixel.style.backgroundColor = 'black';
 
-const bluePixel = document.getElementById('blue');
-bluePixel.style.backgroundColor = 'blue';
+  const bluePixel = document.getElementById('blue');
+  bluePixel.style.backgroundColor = 'blue';
 
-const indigoPixel = document.getElementById('indigo');
-indigoPixel.style.backgroundColor = 'indigo';
+  const indigoPixel = document.getElementById('indigo');
+  indigoPixel.style.backgroundColor = 'indigo';
 
-const greenPixel = document.getElementById('green');
-greenPixel.style.backgroundColor = 'green';
+  const greenPixel = document.getElementById('green');
+  greenPixel.style.backgroundColor = 'green';
 
-let selectedColor = blackPixel.style.backgroundColor;
-
-function colorSelect(colorPixel) {
-  selectedColor = colorPixel.style.backgroundColor;
-}
-
-blackPixel.addEventListener('click', function () {
-  colorSelect(blackPixel);
-  blackPixel.className = 'selected';
-  bluePixel.className = 'color';
-  greenPixel.className = 'color';
-  indigoPixel.className = 'color';
-});
-
-bluePixel.addEventListener('click', function () {
-  colorSelect(bluePixel);
-  blackPixel.className = 'color';
-  bluePixel.className = 'selected';
-  greenPixel.className = 'color';
-  indigoPixel.className = 'color';
-});
-
-greenPixel.addEventListener('click', function () {
-  colorSelect(greenPixel);
-  blackPixel.className = 'color';
-  bluePixel.className = 'color';
-  greenPixel.className = 'selected';
-  indigoPixel.className = 'color';
-});
-
-indigoPixel.addEventListener('click', function () {
-  colorSelect(indigoPixel);
-  blackPixel.className = 'color';
-  bluePixel.className = 'color';
-  greenPixel.className = 'color';
-  indigoPixel.className = 'selected';
-});
-
-const pixelSelected = document.querySelectorAll('.pixel');
-function paintPixel(index) {
-  pixelSelected[index].addEventListener('click', function () {
-    pixelSelected[index].style.backgroundColor = selectedColor;
+  blackPixel.addEventListener('click', function () {
+    document.querySelector('.selected').classList.remove('selected');
+    blackPixel.classList.add('selected');
   });
-}
 
-for (let index = 0; index < pixelSelected.length; index += 1) {
-  paintPixel(index);
-}
+  bluePixel.addEventListener('click', function () {
+    document.querySelector('.selected').classList.remove('selected');
+    bluePixel.classList.add('selected');
+  });
 
-// blackPixel.addEventListener('click', function(){
-//   selectedColor = document.getElementById('black').style.backgroundColor;
-//   blackPixel.className = 'selected';
-// })
+  greenPixel.addEventListener('click', function () {
+    document.querySelector('.selected').classList.remove('selected');
+    greenPixel.classList.add('selected');
+  });
 
-// bluePixel.addEventListener('click', function(){
-//   selectedColor = document.getElementById('blue').style.backgroundColor;
-//   bluePixel.className = 'selected';
-// })
+  indigoPixel.addEventListener('click', function () {
+    document.querySelector('.selected').classList.remove('selected');
+    indigoPixel.classList.add('selected');
+  });
 
-// greenPixel.addEventListener('click', function(){
-//   selectedColor = document.getElementById('green').style.backgroundColor;
-//   greenPixel.className = 'selected';
-// })
+  const pixelSelected = document.querySelectorAll('.pixel');
+  function paintPixel(index) {
+    pixelSelected[index].addEventListener('click', function () {
+      const selectedColor = document.querySelector('.selected');
+      pixelSelected[index].style.backgroundColor = selectedColor.style.backgroundColor;
+    });
+  }
 
-// indigoPixel.addEventListener('click', function(){
-//   selectedColor = document.getElementById('indigo').style.backgroundColor;
-//   indigoPixel.className = 'selected';
-// })
+  for (let index = 0; index < pixelSelected.length; index += 1) {
+    paintPixel(index);
+  }
