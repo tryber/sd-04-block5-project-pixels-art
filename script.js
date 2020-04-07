57ae66ab7fe854905e1d3ec879ae0e3a71315e4c
Rangel Paolo <paolorangel1996@outlook.com>
2020-04-07 17:57:07 -0300

    Adiconados 25 pixels usando JavaScript.

--- script.js
+++ script.js
@@ -1,24 +1,37 @@
-const paleta = document.querySelectorAll('.color'); // Manipula as cores da paleta
+const paleta = document.querySelectorAll('.color'); // Manipula as cores da paleta.
+const quadro = document.getElementById('pixel-board'); // Manipula o quadro de pixels.
 
-window.onload = function() {
-    carregaCores();
+function carregaCores () {
+  for (let number = 0; number < paleta.length; number+=1) {
+    switch (number) {
+      case 0:
+        paleta[number].style.backgroundColor = 'black';
+        break;
+      case 1:
+        paleta[number].style.backgroundColor = 'red';
+        break;
+      case 2:
+        paleta[number].style.backgroundColor = 'blue';
+        break;
+      case 3:
+        paleta[number].style.backgroundColor = 'green';
+        break;
+      default:
+        console.log('Erro: nenhuma cor adicionar para os elementos div.');
+    }
+  }
 }
 
-function carregaCores() {
-    for(let number = 0; number < paleta.length; number++) {
-        switch(number) {
-            case 0:
-                paleta[number].style.backgroundColor = 'black';
-                break;
-            case 1:
-                paleta[number].style.backgroundColor = 'red';
-                break;
-            case 2:
-                paleta[number].style.backgroundColor = 'blue';
-                break;
-            case 3:
-                paleta[number].style.backgroundColor = 'green';
-                break;
-        }
-    }
+function carregaPixels () {
+	for (let pixel = 0; pixel < 25; pixel+=1) {
+		let quadrinho = document.createElement('div'); // Cria o pixel.
+		quadrinho.className = 'pixel'; // Configura atributo class.
+		quadrinho.style.backgroundColor = 'white'; // Configura cor de fundo para branco.
+		quadro.appendChild(quadrinho); // Adiciona no quadro.
+	}
+}
+
+window.onload = function () {
+	carregaCores ();
+	carregaPixels ();
 }
