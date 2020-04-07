0218e640011f8c5439fa32f78a20ef26dac05368
Caio Magalhaes <caio.lmf@gmail.com>
2020-04-07 17:47:29 -0300

    Add random color palette feature

--- script.js
+++ script.js
@@ -4,7 +4,12 @@ colorPalette.forEach((color) => {
   if (color.classList.contains('black')) {
     color.classList.add('selected');
     selectedColor = color;
-  } 
+  } else {
+    const red = Math.round(Math.random()*255);
+    const green = Math.round(Math.random()*255);
+    const blue = Math.round(Math.random()*255);
+    color.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+  }
   color.addEventListener('click', function(e) {
     if (selectedColor) {
       selectedColor.classList.remove('selected'); 
@@ -17,6 +22,7 @@ colorPalette.forEach((color) => {
 createBoard(5);
 function createBoard(pixels) {
   const pixelBoard = document.querySelector('#pixel-board');
+  pixelBoard.innerHTML = '';
   for (let i = 0; i < pixels  ; i++) {
     const pixelRow = document.createElement('div');
     pixelRow.className = 'pixel-row flex';
@@ -39,4 +45,16 @@ clearBtn.addEventListener('click', function(e) {
   pixels.forEach(pixel => {
     pixel.style.backgroundColor = 'rgb(255, 255, 255)';
   });
+});
+
+const generateBtn = document.querySelector('#generate-board');
+generateBtn.addEventListener('click', function (e) {
+  const boardSize = document.querySelector('#board-size').value;
+  if (boardSize > 50) {
+    createBoard(50);
+  } else if (boardSize < 5) {
+    createBoard(5);
+  } else {
+    createBoard(boardSize);
+  };
 });
