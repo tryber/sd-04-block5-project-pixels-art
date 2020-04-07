80d4bc2fd212f27668ad0e407405da3eaf8c5409
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-04-07 15:50:31 -0300

    add function that change the class selected

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 window.onload = () => {
   const arrayColors = ['black', 'red', 'green', 'blue'];
   const elementsColor = document.getElementsByClassName('color');
+  const palette = document.getElementById('color-palette')
   // paint the elements background
   for (let i = 0; i < elementsColor.length; i += 1) {
     elementsColor[i].style.backgroundColor = arrayColors[i];
   }
+  
+  palette.addEventListener('click', (event) => {
+    document.querySelector('.selected').classList.remove('selected');
+    event.target.classList.add('selected');
+  })
+  
 }
