473bfc9c752367ae8230c64598c9521600005f0d
Diego Matheus Campos <diegodiko05@gmail.com>
2020-04-07 21:39:25 -0300

    Botao ClearBoard add com a funcçao de limpar os pixels

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const colorSelected = document.getElementsByClassName('color');
 const pixelBoard = document.getElementById('pixel-board');
+const pixelColor = document.getElementsByClassName('pixel');
+const clear = document.getElementById('clear-board');
 // Let pois vai mudar no evento e.target
 let selectedColor = 'black';
 sessionStorage.setItem('color', 'black');
 // Seletor da cor
 const selecaoTabela = function (e) {
   selectedColor = window.getComputedStyle(e.target).getPropertyValue('background-color');
-    // Selecao da classe
+  // Selecao da classe
   for (let i = 0; i < colorSelected.length; i += 1) {
     colorSelected[i].classList.remove('selected');
   }
@@ -24,3 +26,11 @@ for (let i = 0; i < colorSelected.length; i += 1) {
 }
 // chamando a funcao pelo click
 pixelBoard.addEventListener('click', colorSelect);
+
+function clearBoard() {
+  for (let i = 0; i < pixelColor.length; i += 1) {
+    pixelColor[i].style.backgroundColor = 'white';
+    pixelColor[i].style.borderColor = 'black'
+  }
+}
+clear.addEventListener('click', clearBoard);
