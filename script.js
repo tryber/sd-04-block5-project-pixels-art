f889015010c5ee8b206bf3966d144b5365cf4a1f
orlandomessias <programadorlando@gmail.com>
2020-04-08 17:39:20 -0300

    solving code climate issues

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ const pixelsPallete = document.querySelectorAll('.color');
 let color = 'rgb(0, 0, 0)';
 const pixels = document.querySelectorAll('.pixel');
 const resetBtn = document.getElementById('clear-board');
-let arrayColors = ['black'];
+const arrayColors = ['black'];
 let randomic = 0;
 
 function randomColors() {
   for (let i = 1; i <= 3; i += 1) {
-      randomic = Math.floor(Math.random() * 1531334);
-      arrayColors[i] = '#' + randomic;
+    randomic = Math.floor(Math.random() * 1531334);
+    arrayColors[i] = '#' + randomic;
   }
 }
 
@@ -20,28 +20,28 @@ for (let i = 0; i < arrayColors.length; i += 1) {
 
 function removeClass() {
   for (let i = 0; i < pixelsPallete.length; i += 1) {
-      pixelsPallete[i].classList.remove('selected');
+    pixelsPallete[i].classList.remove('selected');
   }
 }
 
 for (let i = 0; i < pixelsPallete.length; i += 1) {
   pixelsPallete[i].addEventListener('click', () => {
-      if (!pixelsPallete[i].classList.contains('selected')) {
-          removeClass();
-          color = window.getComputedStyle(pixelsPallete[i], null).getPropertyValue('background-color');
-          pixelsPallete[i].classList.add('selected');
+    if (!pixelsPallete[i].classList.contains('selected')) {
+        removeClass();
+        color = window.getComputedStyle(pixelsPallete[i], null).getPropertyValue('background-color');
+        pixelsPallete[i].classList.add('selected');
       }
   });
 }
 
 for (let i = 0; i < pixels.length; i += 1) {
   pixels[i].addEventListener('click', () => {
-      pixels[i].style.backgroundColor = color;
+    pixels[i].style.backgroundColor = color;
   });
 }
 
 resetBtn.addEventListener('click', () => {
   for (let i = 0; i < pixels.length; i += 1) {
-      pixels[i].style.backgroundColor = 'white';
+    pixels[i].style.backgroundColor = 'white';
   }
 });
