6cd3a46ab8a97eee246faf1c5ddd5e5e8a846f62
juniomelos <juniomelos@gmail.com>
2020-04-08 18:49:54 -0300

    Running code and some codeclimate issues 1

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ let redColor = document.getElementById("red");
 let greenColor = document.getElementById("green");
 let clearBoard = document.getElementById("clear-board");
 let pixelBoard = document.getElementsByClassName("pixel");
-let colors = document.getElementsByClassName("color");
 
 blackColor.style.background = "black";
 purpleColor.style.background = "purple";
@@ -21,28 +20,30 @@ function saveColor(n) {
   let color = n;
   localStorage.removeItem('color');
   localStorage.setItem('color', color);
-};
+}
 
 window.onload = function () {
   saveColor("black");
   black.className = "color selected";
 };
 
+let colors = document.getElementsByClassName("color");
 for (let i = 0; i < colors.length; i += 1) {
   colors[i].addEventListener('click', function (event) {
     let colorsValue = event.target.style.backgroundColor;
     let colorsClassValue = colors[i].className;
     for (let i = 0; i < colors.length; i += 1) {
+      colors[i].classList.remove("selected");           
     }
     colors[i].classList.add("selected");                       
   })
-};
+}
 
 let pixels = document.querySelectorAll(".pixel");
-for (let i = 0; i < pixels.length; i += 1) {
+for (let i = 0; i < pixels.length; i += 1) {                     
   pixels[i].addEventListener('click', function (event) {
     let selectedColor = document.querySelector(".selected");
     let colorsSelected = selectedColor.style.backgroundColor;
     pixels[i].style.backgroundColor = colorsSelected;
   })
-};
+}
