a2dd1e7cffbafb93604800928b4d48a85a62c9af
Fabiola Deiro <fabioladeiro7@gmail.com>
2020-04-08 15:05:31 -0300

    Tentativa de passar pelo requisito 9

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ firstColor.addEventListener("click", function() {
   document.getElementById("secondColor").className = "color";
   document.getElementById("thirdColor").className = "color";
   document.getElementById("fourthColor").className = "color";
+  document.querySelector(".selected").style.backgroundColor = "black";
+  console.log(document.querySelector(".selected").style.backgroundColor);
 });
 
 let secondColor = document.getElementById("secondColor");
@@ -17,6 +19,7 @@ secondColor.addEventListener("click", function() {
   document.getElementById("thirdColor").className = "color";
   document.getElementById("fourthColor").className = "color";
   secondColor.className = "color selected";
+  document.querySelector(".selected").style.backgroundColor = "green";
 });
 
 let thirdColor = document.getElementById("thirdColor");
@@ -25,6 +28,7 @@ thirdColor.addEventListener("click", function() {
   document.getElementById("secondColor").className = "color";
   document.getElementById("fourthColor").className = "color";
   thirdColor.className = "color selected";
+  document.querySelector(".selected").style.backgroundColor = "blue";
 });
 
 let fourthColor = document.getElementById("fourthColor");
@@ -33,4 +37,18 @@ fourthColor.addEventListener("click", function() {
   document.getElementById("secondColor").className = "color";
   document.getElementById("thirdColor").className = "color";
   fourthColor.className = "color selected";
+  document.querySelector(".selected").style.backgroundColor = "red";
 });
+
+document.getElementById("firstColor").style.backgroundColor = "black";
+document.getElementById("secondColor").style.backgroundColor = "green";
+document.getElementById("thirdColor").style.backgroundColor = "blue";
+document.getElementById("fourthColor").style.backgroundColor = "red";
+
+
+let pixelBoard = document.querySelector("#pixel-board");
+pixelBoard.addEventListener("click", function(event){
+  let selectedColor = document.querySelector(".selected").style.backgroundColor;
+  console.log(document.querySelector(".selected").style.backgroundColor);
+  event.target.style.backgroundColor = selectedColor;
+  });   
