a489b6508430c5feab3d8ab8af562132f469ad5e
mathieu bouhelier <mathieubouhelier@gmail.com>
2020-04-08 12:00:16 -0300

    add remove and add .selected to color elements

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 window.onload = function(){
+    let colors = document.querySelectorAll(".color"); 
 
+    for (let i=0; i<colors.length; i+=1){
+    colors[i].addEventListener('click',function(event){
+        let colorsValue = event.target.style.backgroundColor;
+        let colorsClassValue = colors[i].className;
+        // loop to remove class .selected to all
+        for (let i=0; i<colors.length; i+=1){
+            colors[i].classList.remove("selected");
+        }
+        colors[i].classList.add("selected");//add the class selected to the clicked 
+        
+    })
+ 
+}
 }
