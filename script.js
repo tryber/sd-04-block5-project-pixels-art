9e83f6031920992f2e20caf85f423f5aa10b52be
Fabiola Deiro <fabioladeiro7@gmail.com>
2020-04-08 16:03:15 -0300

    Tentativa de passar pelo requisito 11

--- script.js
+++ script.js
@@ -1,54 +1,62 @@
-window.onload = blackColor;
-function blackColor () {
-  document.getElementById("firstColor").className = "color selected";
+function blackColor() {
+  document.getElementById('firstColor').className = 'color selected';
 }
 
-let firstColor = document.getElementById("firstColor");
-firstColor.addEventListener("click", function() {
-  document.getElementById("firstColor").className = "color selected";
-  document.getElementById("secondColor").className = "color";
-  document.getElementById("thirdColor").className = "color";
-  document.getElementById("fourthColor").className = "color";
-  document.querySelector(".selected").style.backgroundColor = "black";
-  console.log(document.querySelector(".selected").style.backgroundColor);
+window.onload = blackColor;
+
+const firstColor = document.getElementById('firstColor');
+firstColor.addEventListener('click', function () {
+  document.getElementById('firstColor').className = 'color selected';
+  document.getElementById('secondColor').className = 'color';
+  document.getElementById('thirdColor').className = 'color';
+  document.getElementById('fourthColor').className = 'color';
+  document.querySelector('.selected').style.backgroundColor = 'black';
 });
 
-let secondColor = document.getElementById("secondColor");
-secondColor.addEventListener("click", function() {
-  document.getElementById("firstColor").className = "color";
-  document.getElementById("thirdColor").className = "color";
-  document.getElementById("fourthColor").className = "color";
-  secondColor.className = "color selected";
-  document.querySelector(".selected").style.backgroundColor = "green";
+const secondColor = document.getElementById('secondColor');
+secondColor.addEventListener('click', function () {
+  document.getElementById('firstColor').className = 'color';
+  document.getElementById('thirdColor').className = 'color';
+  document.getElementById('fourthColor').className = 'color';
+  secondColor.className = 'color selected';
+  document.querySelector('.selected').style.backgroundColor = 'green';
 });
 
-let thirdColor = document.getElementById("thirdColor");
-thirdColor.addEventListener("click", function() {
-  document.getElementById("firstColor").className = "color";
-  document.getElementById("secondColor").className = "color";
-  document.getElementById("fourthColor").className = "color";
-  thirdColor.className = "color selected";
-  document.querySelector(".selected").style.backgroundColor = "blue";
+const thirdColor = document.getElementById('thirdColor');
+thirdColor.addEventListener('click', function () {
+  document.getElementById('firstColor').className = 'color';
+  document.getElementById('secondColor').className = 'color';
+  document.getElementById('fourthColor').className = 'color';
+  thirdColor.className = 'color selected';
+  document.querySelector('.selected').style.backgroundColor = 'blue';
 });
 
-let fourthColor = document.getElementById("fourthColor");
-fourthColor.addEventListener("click", function() {
-  document.getElementById("firstColor").className = "color";
-  document.getElementById("secondColor").className = "color";
-  document.getElementById("thirdColor").className = "color";
-  fourthColor.className = "color selected";
-  document.querySelector(".selected").style.backgroundColor = "red";
+const fourthColor = document.getElementById('fourthColor');
+fourthColor.addEventListener('click', function () {
+  document.getElementById('firstColor').className = 'color';
+  document.getElementById('secondColor').className = 'color';
+  document.getElementById('thirdColor').className = 'color';
+  fourthColor.className = 'color selected';
+  document.querySelector('.selected').style.backgroundColor = 'red';
 });
 
-document.getElementById("firstColor").style.backgroundColor = "black";
-document.getElementById("secondColor").style.backgroundColor = "green";
-document.getElementById("thirdColor").style.backgroundColor = "blue";
-document.getElementById("fourthColor").style.backgroundColor = "red";
+document.getElementById('firstColor').style.backgroundColor = 'black';
+document.getElementById('secondColor').style.backgroundColor = 'green';
+document.getElementById('thirdColor').style.backgroundColor = 'blue';
+document.getElementById('fourthColor').style.backgroundColor = 'red';
 
-
-let pixelBoard = document.querySelector("#pixel-board");
-pixelBoard.addEventListener("click", function(event){
-  let selectedColor = document.querySelector(".selected").style.backgroundColor;
-  console.log(document.querySelector(".selected").style.backgroundColor);
+const pixelBoard = document.querySelector('#pixel-board');
+pixelBoard.addEventListener('click', function (event) {
+  const selectedColor = document.querySelector('.selected').style.backgroundColor;
   event.target.style.backgroundColor = selectedColor;
-  });   
+  document.getElementById('pixel-board').style.backgroundColor = 'white';
+});
+
+const botao = document.querySelector('.botao');
+const pixel = document.querySelectorAll('.pixel');
+botao.addEventListener('click', function () {
+  document.getElementById('pixel-board').style.backgroundColor = 'white';
+  for (let i = 0; i < pixel.length; i = 1 + i) {
+    pixel[i].style.backgroundColor = 'white';
+  }
+});
