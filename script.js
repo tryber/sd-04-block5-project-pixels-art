2c561fa61cb39a8bf9b3ea500cca0293c2ba0b9f
juniomelos <juniomelos@gmail.com>
2020-04-08 09:46:15 -0300

    button clear to try to go through item 5

--- script.js
+++ script.js
@@ -2,11 +2,20 @@ let blackColor = document.getElementById("black");
 let purpleColor = document.getElementById("purple");
 let redColor = document.getElementById("red");
 let greenColor = document.getElementById("green");
+let clearBoard = document.getElementById("clear-board");
+let pixelBoard = document.getElementsByClassName("pixel");
 
 blackColor.style.background = "black";
 purpleColor.style.background = "purple";
 redColor.style.background = "red";
 greenColor.style.background = "green";
+
+clearBoard.addEventListener("click", function(){
+  for (i = 0; i < pixelBoard.length; i += 1){
+    pixelBoard[i].style.background = "white";
+  }
+});
+
 
 
 
