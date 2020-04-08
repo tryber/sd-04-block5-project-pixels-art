16c2f2186c61c10bd8594309f76925cbad296da5
Diego Matheus Campos <diegodiko05@gmail.com>
2020-04-07 21:00:12 -0300

    alteracao do nome da funçao para atender requisito do cyber

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-const colorSelect = document.getElementsByClassName('color');
+const colorSelected = document.getElementsByClassName('color');
 const pixelBoard = document.getElementById('pixel-board');
 //Let pois vai mudar no evento e.target
-let selecaoCores = 'black';
+let selectedColor = 'black';
 sessionStorage.setItem('color', 'black');
 // Seletor da cor
 const selecaoTabela = function (e) {
-  selecaoCores = window.getComputedStyle(e.target).getPropertyValue('background-color');
+  selectedColor = window.getComputedStyle(e.target).getPropertyValue('background-color');
     // Selecao da classe
-  for (let i = 0; i < colorSelect.length; i += 1) {
-    colorSelect[i].classList.remove('selected');
+  for (let i = 0; i < colorSelected.length; i += 1) {
+    colorSelected[i].classList.remove('selected');
   }
   e.target.classList.add('selected');
 };
 
 // Pintar o quadro
-function pintaPixel(e) {
-  e.target.style.backgroundColor = selecaoCores;
-  e.target.style.borderColor = selecaoCores;
+function colorSelect(e) {
+  e.target.style.backgroundColor = selectedColor;
+  e.target.style.borderColor = selectedColor;
 }
 //for para cada quadradinho
-for (let i = 0; i < colorSelect.length; i += 1) {
-  colorSelect[i].addEventListener('click', selecaoTabela);
+for (let i = 0; i < colorSelected.length; i += 1) {
+  colorSelected[i].addEventListener('click', selecaoTabela);
 }
 //chamando a funcao pelo click
-pixelBoard.addEventListener('click', pintaPixel);
+pixelBoard.addEventListener('click', colorSelect);
