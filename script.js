ff05c6bd48dbde09a70c87961a72d91d4702833a
jessica-tavares <jessicatavareseel@gmail.com>
2020-04-07 21:28:45 -0300

    colorindo os pixels, falta funcionar o clear

--- script.js
+++ script.js
@@ -5,12 +5,13 @@ const pink = document.getElementsByClassName('color')[2];
 const yellow = document.getElementsByClassName('color')[3];
 const clearButton = document.getElementById('clear-board');
 const pixel = [];
-for (let i=0; i < 25; i+=1){
+for (let i = 0; i < 25; i += 1){
   pixel[i] = document.getElementsByClassName('pixel')[i];
 }
 
+
 // Funçoes
-localStorage.setItem('color', 'black');
+localStorage.setItem("black", 'black');
 
 function saveColor(n) {
   let color = n;
@@ -20,21 +21,33 @@ function saveColor(n) {
 }
 
 function clearBoard() {
-  localStorage.clear();
+  for (let k = 0; k < 25; k += 1){
+    pixel[k].style.backgroundColor = "white";
+  }
 }
 
 function changeColor(j) {
-  pixel[j].style.backgoundColor = localStorage.getItem("color");
+  pixel[j].style.backgroundColor = localStorage.getItem("color");
 }
 
-window.onload = function() {
 // Criando os event Listener
-black.addEventListener('click', saveColor('black'));
-purple.addEventListener('click', saveColor('blueviolet'));
-pink.addEventListener('click', saveColor('rgb(230, 47, 160)'));
-yellow.addEventListener('click', saveColor('yellow'));
+black.addEventListener('click', function() {
+  saveColor('black');
+})
+purple.addEventListener('click', function() {
+  saveColor('blueviolet');
+})
+pink.addEventListener('click', function() {
+  saveColor('rgb(230, 47, 160)');
+})
+yellow.addEventListener('click', function() {
+  saveColor('yellow');
+})
 clearButton.addEventListener('click', clearBoard());
-for (let j=0; j<25; j+=1){
-  pixel[j].addEventListener('click', changeColor(j));
-}
+
+for (let j = 0; j < 25; j += 1) {
+  pixel[j].addEventListener('click', function() {
+    changeColor(j);
+  })
+  console.log(pixel[j]);
 }
