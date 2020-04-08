338da1e86cc0212ff57bbf448d8ca31ebee28ae3
Diego Matheus Campos <diegodiko05@gmail.com>
2020-04-08 10:23:36 -0300

    Na linha 11 do JS foi acrescentado colorSelected[1], para testar cypress

--- script.js
+++ script.js
@@ -1,31 +1,29 @@
-window.onload = function () {
-  const colorSelected = document.getElementsByClassName('color');
-  const pixelBoard = document.getElementById('pixel-board');
-  const pixelColor = document.getElementsByClassName('pixel');
-  const clear = document.getElementById('clear-board');
-  let selectedColor = 'black';
-  sessionStorage.setItem('color', 'black');
-  const selecaoTabela = function (e) {
-    selectedColor = window.getComputedStyle(e.target).getPropertyValue('background-color');
-    for (let i = 0; i < colorSelected.length; i += 1) {
-      colorSelected[i].classList.remove('selected');
-      e.target.classList.add('selected');
-    }
-  };
-  function colorSelect(e) {
-    e.target.style.backgroundColor = selectedColor;
-    e.target.style.borderColor = selectedColor;
-  }
-  function clearBoard() {
-    for (let i = 0; i < pixelColor.length; i += 1) {
-      pixelColor[i].style.backgroundColor = 'white';
-      pixelColor[i].style = pixelColor;
-    }
-  }
-  sessionStorage.setItem('color', 'black');
+const colorSelected = document.getElementsByClassName('color');
+const pixelBoard = document.getElementById('pixel-board');
+const pixelColor = document.getElementsByClassName('pixel');
+const clear = document.getElementById('clear-board');
+let selectedColor = 'black';
+sessionStorage.setItem('color', 'black');
+const selecaoTabela = function (e) {
+  selectedColor = window.getComputedStyle(e.target).getPropertyValue('background-color');
   for (let i = 0; i < colorSelected.length; i += 1) {
-    colorSelected[i].addEventListener('click', selecaoTabela);
+    colorSelected[i].classList.remove('selected');
+    colorSelected[i].e.target.classList.add('selected');
   }
-  clear.addEventListener('click', clearBoard);
-  pixelBoard.addEventListener('click', colorSelect);
 };
+function colorSelect(e) {
+  e.target.style.backgroundColor = selectedColor;
+  e.target.style.borderColor = selectedColor;
+}
+function clearBoard() {
+  for (let i = 0; i < pixelColor.length; i += 1) {
+    pixelColor[i].style.backgroundColor = 'white';
+    pixelColor[i].style = pixelColor;
+  }
+}
+sessionStorage.setItem('color', 'black');
+for (let i = 0; i < colorSelected.length; i += 1) {
+  colorSelected[i].addEventListener('click', selecaoTabela);
+}
+clear.addEventListener('click', clearBoard);
+pixelBoard.addEventListener('click', colorSelect);
