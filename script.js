906b6b2af6688b7e42e71f8c031a0652c6533a4a
joaosoutto <joaosoutto92@gmail.com>
2020-04-08 19:22:50 -0300

    tentando adicionar a função pra colorir os px

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
-window.onload = function () {
-  let color1 = document.getElementsByClassName('color')[0]; color1.style.backgroundColor = 'black';
-  let color2 = document.getElementsByClassName('color')[1]; color2.style.backgroundColor = 'olive';
-  let color3 = document.getElementsByClassName('color')[2]; color3.style.backgroundColor = 'red';
-  let color4 = document.getElementsByClassName('color')[3]; color4.style.backgroundColor = 'navy';
-};
+
+  
+  let blackPx = document.getElementById('black'); blackPx.style.backgroundColor = "black";
+  let olivePx = document.getElementById('olive'); olivePx.style.backgroundColor = 'olive';
+  let redPx = document.getElementById('red'); redPx.style.backgroundColor = 'red';
+  let navyPx = document.getElementById('navy'); navyPx.style.backgroundColor = 'navy';
+  
+  
+  blackPx.addEventListener('click', function () {
+    document.querySelector('.selected').classList.remove('selected');
+    blackPx.classList.add('selected');
+  });
+  olivePx.addEventListener('click', function () {
+    document.querySelector('.selected').classList.remove('selected');
+    olivePx.classList.add('selected');
+  });
+  
+  redPx.addEventListener('click', function () {
+    document.querySelector('.selected').classList.remove('selected');
+    redPx.classList.add('selected');
+  });
+  
+  navyPx.addEventListener('click', function () {
+    document.querySelector('.selected').classList.remove('selected');
+    navyPx.classList.add('selected');
+  });
+
+  let selectedPx = document.querySelectorAll('.pixel');
+  function paintPx(index) {
+  selectedPx[index].addEventListener('click', function () {
+    let selectedColor = document.querySelector('.selected');
+    selectedPx[index].style.backgroundColor =
+      selectedColor.style.backgroundColor;
+  });
+  };
+
+  for (let index = 0; index < selectedPx.length; index++) {
+  paintPx(index);
+  }
+
+  
