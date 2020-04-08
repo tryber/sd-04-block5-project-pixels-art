52ceedae873554a97e585032cc5e88b582af83e1
orlandomessias <programadorlando@gmail.com>
2020-04-08 17:00:40 -0300

    create function to random colors I

--- script.js
+++ script.js
@@ -3,35 +3,20 @@ window.onload = () => {
     let color = 'rgb(0, 0, 0)';
     const pixels = document.querySelectorAll('.pixel');
     const resetBtn = document.getElementById('clear-board');
-    let arrayColors = ['black', 'red', 'green', 'blue'];
+    let arrayColors = ['black'];
     let randomic = 0;
-    let arrayRandom = [0];
-    let countEqual = 0;
-    let newRandom = true;
 
     function randomColors(){
         for (let i = 1; i <= 3; i += 1) {
-        while (newRandom === true) {
-            randomic = Math.floor(Math.random() * 3) + 1;
-            countEqual = 0;
-            for (let j = 1; j <= arrayRandom.length; j += 1) {
-                if (randomic === arrayRandom[j]) {
-                    countEqual += 1;
-                }
-            }
-            if (countEqual === 0) {
-                    newRandom = false;
-            }
-        }
-        arrayRandom[i] = randomic;
-        newRandom = true;
+            randomic = Math.floor(Math.random() * 1531334);
+            arrayColors[i] = '#' + randomic;
         }
     }
 
     randomColors();
 
     for (let i = 0; i < arrayColors.length; i += 1) {
-    pixelsPallete[i].style.backgroundColor = arrayColors[arrayRandom[i]];
+    pixelsPallete[i].style.backgroundColor = arrayColors[i];
     }
 
     function removeClass() {
