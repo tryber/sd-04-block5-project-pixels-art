65206ea80d2e2ff85a31b4bb01be760c3fd1a18f
igorgbr <igorgphotoarte@gmail.com>
2020-04-08 01:41:14 -0300

    primeiro commit com alteraçoes basicas

---
+++ script.js
@@ -0,0 +1,13 @@
+//cores
+colorBlack = document.querySelector("#color-black");
+colorRed = document.querySelector("#color-red");
+colorBlue = document.querySelector("#color-blue");
+colorGreen = document.querySelector("#color-green");
+
+pixelBoard = document.querySelector("#pixel-board")
+pixel = document.querySelector(".pixels");
+
+
+// colorBlack.addEventListener('click', function(){
+
+// })
