6df9e409722b25c8ae4f5f4a059227dc6a91c32e
deboracosilveira <deboracosilveira@gmail.com>
2020-04-08 19:50:15 -0300

    requirement-8

--- script.js
+++ script.js
@@ -1,6 +1,48 @@
-  function blackSelected() {
-    let black = document.querySelector(".black");
-    black.classList.add("selected");
-  
-  }
- 
+let black = document.querySelector(".black");
+let blue = document.querySelector(".blue");
+let pink = document.querySelector(".pink");
+let gray = document.querySelector(".gray");
+// Select black when the page load
+window.addEventListener("load", function() {
+  black.classList.add("selected");
+});
+let palette = document.querySelectorAll(".color")
+
+
+function blackSelected() {
+    let item = document.querySelector(".color.selected");
+    if (item) {
+      item.classList.remove("selected");
+      black.classList.add("selected");
+    }
+  console.log(palette);
+};
+
+function blueSelected() {
+  let item = document.querySelector(".color.selected");
+    if (item) {
+      item.classList.remove("selected");
+      blue.classList.add("selected");
+    } 
+  console.log(palette);
+};
+function pinkSelected() {
+  let item = document.querySelector(".color.selected");
+    if (item) {
+      item.classList.remove("selected");
+      pink.classList.add("selected");
+    } 
+  console.log(palette);
+};
+function graySelected() {
+  let item = document.querySelector(".color.selected");
+    if (item) {
+      item.classList.remove("selected");
+      gray.classList.add("selected");
+    } 
+  console.log(palette);
+};
+
+
+
+
