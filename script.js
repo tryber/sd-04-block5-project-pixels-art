3eed5c08c9c52ebbdad92bb218c16535075db852
orlandomessias <programadorlando@gmail.com>
2020-04-08 19:09:03 -0300

    create pixels board on click

--- script.js
+++ script.js
@@ -4,44 +4,60 @@ const pixels = document.querySelectorAll('.pixel');
 const resetBtn = document.getElementById('clear-board');
 const arrayColors = ['black'];
 let randomic = 0;
+const pixelBoard = document.getElementById('pixel-board')
+const boardSize = document.getElementById('board-size')
 
 function randomColors() {
-  for (let i = 1; i <= 3; i += 1) {
-    randomic = Math.floor(Math.random() * 1531334);
-    arrayColors[i] = '#' + randomic;
-  }
+    for (let i = 1; i <= 3; i += 1) {
+        randomic = Math.floor(Math.random() * 1531334);
+        arrayColors[i] = '#' + randomic;
+    }
 }
 
 randomColors();
 
 for (let i = 0; i < arrayColors.length; i += 1) {
-  pixelsPallete[i].style.backgroundColor = arrayColors[i];
+    pixelsPallete[i].style.backgroundColor = arrayColors[i];
 }
 
 function removeClass() {
-  for (let i = 0; i < pixelsPallete.length; i += 1) {
-    pixelsPallete[i].classList.remove('selected');
-  }
+    for (let i = 0; i < pixelsPallete.length; i += 1) {
+        pixelsPallete[i].classList.remove('selected');
+    }
 }
 
 for (let i = 0; i < pixelsPallete.length; i += 1) {
-  pixelsPallete[i].addEventListener('click', () => {
-    if (!pixelsPallete[i].classList.contains('selected')) {
-        removeClass();
-        color = window.getComputedStyle(pixelsPallete[i], null).getPropertyValue('background-color');
-        pixelsPallete[i].classList.add('selected');
-      }
-  });
+    pixelsPallete[i].addEventListener('click', () => {
+        if (!pixelsPallete[i].classList.contains('selected')) {
+            removeClass();
+            color = window.getComputedStyle(pixelsPallete[i], null).getPropertyValue('background-color');
+            pixelsPallete[i].classList.add('selected');
+        }
+    });
 }
 
 for (let i = 0; i < pixels.length; i += 1) {
-  pixels[i].addEventListener('click', () => {
-    pixels[i].style.backgroundColor = color;
-  });
+    pixels[i].addEventListener('click', () => {
+        pixels[i].style.backgroundColor = color;
+    });
 }
 
 resetBtn.addEventListener('click', () => {
-  for (let i = 0; i < pixels.length; i += 1) {
-    pixels[i].style.backgroundColor = 'white';
-  }
+    for (let i = 0; i < pixels.length; i += 1) {
+        pixels[i].style.backgroundColor = 'white';
+    }
 });
+
+document.getElementById('generate-board').addEventListener('click', () => {
+    let size = parseInt(boardSize.value)
+    let insertDivsLine = ''
+    let insertDivsColumn = ''
+    for (let i = 1; i <= size; i++) {
+        insertDivsColumn += `<div class="pixel"></div>`
+        for(let j = 1; j < size; j++){
+            insertDivsLine += `<div class="pixel"></div>`
+        }
+    }
+    pixelBoard.innerHTML = insertDivsColumn + insertDivsLine
+    pixelBoard.style.width = (42 * size) + 'px'
+})
