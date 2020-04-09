7e6fd59a7e9b5d9db3bc1b220f6fecafd2abe51b
Lucas Barbosa Rosa <lucaslucas2008@gmail.com>
2020-04-09 18:50:42 -0300

    changes to code-climate

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-window.onload = function()
+window.onload = function () 
 {
-    let buttonCor1 = document.getElementById("black-color");
-    let buttonCor2 = document.getElementById("color-block1");
-    let buttonCor3 = document.getElementById("color-block2");
-    let buttonCor4 = document.getElementById("color-block3");
+    const buttonCor1 = document.getElementById('black-color');
+    const buttonCor2 = document.getElementById('color-block1');
+    const buttonCor3 = document.getElementById('color-block2');
+    const buttonCor4 = document.getElementById('color-block3');
 
     IniciaPaleta();
 
-    function IniciaPaleta()
+    function IniciaPaleta() 
     {
-        buttonCor1.style.backgroundColor = "black";
-        buttonCor2.style.backgroundColor = "rgb(255,0,0)";
-        buttonCor3.style.backgroundColor = "rgb(0,255,0)";
-        buttonCor4.style.backgroundColor = "rgb(0,0,255)";
+        buttonCor1.style.backgroundColor = 'black';
+        buttonCor2.style.backgroundColor = 'rgb(255,0,0)';
+        buttonCor3.style.backgroundColor = 'rgb(0,255,0)';
+        buttonCor4.style.backgroundColor = 'rgb(0,0,255)';
     }
 }; 
