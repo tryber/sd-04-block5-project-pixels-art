6789830e3ac91720578e74e45e2c31e10c50daff
danilossousa <danilosantossousa@gmail.com>
2020-04-09 18:19:49 -0300

    v0.7.1.4

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ const colRed = document.getElementById('red');
 const colBlue = document.getElementById('blue');
 const colGreen = document.getElementById('green');
 
-window.onload = function () {
-  this.selection();
-};
-
 function selection() {
   colBlack.style.backgroundColor = colBlack.id;
   colRed.style.backgroundColor = colRed.id;
   colBlue.style.backgroundColor = colBlue.id;
   colGreen.style.backgroundColor = colGreen.id;
 }
+
+window.onload = function () {
+  this.selection();
+};
 
 // console.log(colBlack);
