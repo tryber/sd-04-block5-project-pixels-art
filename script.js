4ca0e0bfaa3f6fc6028f1729636f4ffa080eadf9
danilossousa <danilosantossousa@gmail.com>
2020-04-09 17:07:23 -0300

    v0.7.0.1

---
+++ script.js
@@ -0,0 +1,22 @@
+// let i;
+// let colBlack = document.getElementsByClassName("black")[0];
+// let colRed = document.getElementsByClassName("red")[0];
+// let colBlue = document.getElementsByClassName("blue")[0];
+// let colGreen = document.getElementsByClassName("green")[0];
+// let colors = [colBlack, colRed, colBlue, colGreen];
+
+// window.onload = function () {
+//   selection();
+
+//   console.log(colBlack.className);
+// };
+
+// function selection() {
+//   for (i = 0;  i < colors.length; i += 1) {
+//     if (colors[i].className === "color black") {
+//       colors[i].className = "color selected black";
+//     } else {
+//       colors[i].className = colors[i].className;
+//     };
+//   };
+// };
