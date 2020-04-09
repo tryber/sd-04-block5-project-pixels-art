6e3f36ee37650e3600e33cb792537a05238244e7
Jorge Osiro <jojimurakabe@gmail.com>
2020-04-09 16:56:14 -0300

    class change

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 window.onload = function() {
+    let initial = document.querySelector("#black");
+    initial.className = ".color selected"
+
+    for (let c = 0; c < 4; c+=1) {
+        let color = document.querySelectorAll(".color");
+        color[c].addEventListener('click', function(){
+            this.className = ".color selected"
+        })
+        
+    }
     
     for (let i = 0; i < 25; i+= 1) {
         let pixel = document.querySelectorAll(".pixel");
