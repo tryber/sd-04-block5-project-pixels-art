fadeac1a25e2fef1bf255dad4e3062280261b99d
igorgbr <igorgphotoarte@gmail.com>
2020-04-09 00:12:18 -0300

    cores aleatorias

--- script.js
+++ script.js
@@ -4,15 +4,27 @@ colorRed = document.querySelector("#color-red");
 colorBlue = document.querySelector("#color-blue");
 colorGreen = document.querySelector("#color-green");
 
-colorGreen.style.backgroundColor = 'green';
-colorRed.style.backgroundColor = 'red';
-colorBlue.style.backgroundColor = 'blue';
-colorBlack.style.backgroundColor = 'black';
+let var1 = colorGreen.style.backgroundColor = 'green';
+let var2 = colorRed.style.backgroundColor = 'red';
+let var3 = colorBlue.style.backgroundColor = 'blue';
+let var4 = colorBlack.style.backgroundColor = 'black';
 
 pixelBoard = document.querySelector("#pixel-board")
 pixel = document.querySelectorAll(".pixel");
 
+let arrayCores = [var1, var2, var3, var4];
 
+for (i=1; i < arrayCores.length; i++){
+    const corAleatoria1 = arrayCores[Math.floor(Math.random() * arrayCores.length)];
+    const corAleatoria2 = arrayCores[Math.floor(Math.random() * arrayCores.length)];
+    const corAleatoria3 = arrayCores[Math.floor(Math.random() * arrayCores.length)];
+    cor1 = document.querySelector("#color-red");
+    cor2 = document.querySelector("#color-blue");
+    cor3 = document.querySelector("#color-green");
+    cor1.style.backgroundColor = corAleatoria1;
+    cor2.style.backgroundColor = corAleatoria2;
+    cor3.style.backgroundColor = corAleatoria3;
+}
 function apagaClass(){
 colorBlack.className = 'color';
 colorRed.className = 'color'; 
