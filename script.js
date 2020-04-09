3589bf5590976869d049ce6b3da10b3569bc2624
danilossousa <danilosantossousa@gmail.com>
2020-04-09 17:34:07 -0300

    v0.7.1.1

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
-// let i;
-// let colBlack = document.getElementsByClassName("black")[0];
-// let colRed = document.getElementsByClassName("red")[0];
-// let colBlue = document.getElementsByClassName("blue")[0];
-// let colGreen = document.getElementsByClassName("green")[0];
-// let colors = [colBlack, colRed, colBlue, colGreen];
+let colBlack = document.getElementById("black");
+let colRed = document.getElementById("red");
+let colBlue = document.getElementById("blue");
+let colGreen = document.getElementById("green");
 
-// window.onload = function () {
-//   selection();
+window.onload = function () {
+  selection();
+};
 
-//   console.log(colBlack.className);
-// };
+function selection() {
+  colBlack.style.backgroundColor = colBlack.id;
+  colRed.style.backgroundColor = colRed.id;
+  colBlue.style.backgroundColor = colBlue.id;
+  colGreen.style.backgroundColor = colGreen.id;
+};
 
-// function selection() {
-//   for (i = 0;  i < colors.length; i += 1) {
-//     if (colors[i].className === "color black") {
-//       colors[i].className = "color selected black";
-//     } else {
-//       colors[i].className = colors[i].className;
-//     };
-//   };
-// };
+//console.log(colBlack);
