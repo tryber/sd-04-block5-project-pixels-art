1776bf741b4e918674d5ddfdd9c32a66b86f9903
Ana Higo <anahigo.arq@gmail.com>
2020-04-08 22:38:14 -0300

    tentativa8

--- script.js
+++ script.js
@@ -1,51 +1,50 @@
-let firstColor = document.getElementById("firstColor");
-let secondColor = document.getElementById("secondColor");
-let thirdColor = document.getElementById("thirdColor");
-let fourthColor = document.getElementById("fourthColor");
+let palette = document.querySelectorAll('.color');
+let firstColor = document.querySelectorAll('.color')[0];
+let secondColor = document.querySelectorAll('.color')[1];
+let thirdColor = document.querySelectorAll('.color')[2];
+let fourthColor = document.querySelectorAll('.color')[3];
 
-firstColor.style.backgroundColor = "black";
-secondColor.style.backgroundColor = "blue";
-thirdColor.style.backgroundColor = "yellow";
-fourthColor.style.backgroundColor = "red";
+firstColor.style.backgroundColor = 'black';
+secondColor.style.backgroundColor = 'blue';
+thirdColor.style.backgroundColor = 'yellow';
+fourthColor.style.backgroundColor = 'red';
 
 window.addEventListener("load", function() {
-  firstColor.classList.add("colorSelected");
-});
+  firstColor.classList.add('colorSelected');
+})
+
+const color = document.querySelector('.colorSelected');
 
 function firstColorSelected() {
-  let color = document.querySelector(".colorSelected");
-    if (color) {
-      color.classList.remove("colorSelected");
-      firstColor.classList.add("colorSelected");
-    }
-};
+  if (color) {
+    color.classList.remove('colorSelected');
+    firstColor.classList.add('colorSelected');
+  }
+}
 
 function secondColorSelected() {
-  let color = document.querySelector(".colorSelected");
-    if (color) {
-      color.classList.remove("colorSelected");
-      secondColor.classList.add("colorSelected");
-    } 
-};
+  if (color) {
+    color.classList.remove('colorSelected');
+    secondColor.classList.add('colorSelected');
+  } 
+}
 
 function thirdColorSelected() {
-  let color = document.querySelector(".colorSelected");
-    if (color) {
-      color.classList.remove("colorSelected");
-      thirdColor.classList.add("colorSelected");
-    } 
-};
+  if (color) {
+    color.classList.remove('colorSelected');
+    thirdColor.classList.add('colorSelected');
+  }
+}
 
 function fourthColorSelected() {
-  let color = document.querySelector(".colorSelected");
-    if (color) {
-      color.classList.remove("colorSelected");
-      fourthColor.classList.add("colorSelected");
-    } 
-};
+  if (color) {
+    color.classList.remove('colorSelected');
+    fourthColor.classList.add("colorSelected");
+  } 
+}
 
-let pixelBoard = document.getElementById("pixel-board");
+const pixelBoard = document.getElementById('pixel-board');
 
-pixelBoard.addEventListener("click", function(changeColor) {
-  changeColor.target.style.backgroundColor = document.querySelector(".colorSelected").style.backgroundColor;
-});
+pixelBoard.addEventListener('click' function (changeColor) {
+  changeColor.target.style.backgroundColor = document.querySelector('.colorSelected').style.backgroundColor;
+})
