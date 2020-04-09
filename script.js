5d416e66dd67b97ffa836b25c591f9474622d974
Ana Higo <anahigo.arq@gmail.com>
2020-04-08 23:18:15 -0300

    tentativa11

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-let palette = document.querySelectorAll('.color');
-let firstColor = document.querySelectorAll('.color')[0];
-let secondColor = document.querySelectorAll('.color')[1];
-let thirdColor = document.querySelectorAll('.color')[2];
-let fourthColor = document.querySelectorAll('.color')[3];
+const palette = document.querySelectorAll('.color');
+const firstColor = document.querySelectorAll('.color')[0];
+const secondColor = document.querySelectorAll('.color')[1];
+const thirdColor = document.querySelectorAll('.color')[2];
+const fourthColor = document.querySelectorAll('.color')[3];
 
 firstColor.style.backgroundColor = 'black';
 secondColor.style.backgroundColor = 'blue';
 thirdColor.style.backgroundColor = 'yellow';
 fourthColor.style.backgroundColor = 'red';
 
-window.addEventListener("load", function() {
+window.addEventListener('load', function() {
   firstColor.classList.add('colorSelected');
-})
+});
 
 function firstColorSelected() {
   const color = document.querySelector('.colorSelected');
@@ -41,12 +41,12 @@ function fourthColorSelected() {
   const color = document.querySelector('.colorSelected');
   if (color) {
     color.classList.remove('colorSelected');
-    fourthColor.classList.add("colorSelected");
+    fourthColor.classList.add('colorSelected');
   }
 }
 
 const pixelBoard = document.getElementById('pixel-board');
 
-pixelBoard.addEventListener('click', function(changeColor) {
+pixelBoard.addEventListener('click', function (changeColor) {
   changeColor.target.style.backgroundColor = document.querySelector('.colorSelected').style.backgroundColor;
-})
+});
