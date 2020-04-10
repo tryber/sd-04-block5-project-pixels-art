2a63488ee21778c0f14dabc981d42e0dcdd68136
Lucas Barbosa Rosa <lucaslucas2008@gmail.com>
2020-04-10 19:37:42 -0300

    Add requisition 7-8

--- script.js
+++ script.js
@@ -5,9 +5,27 @@ window.onload = function ()
     const buttonCor3 = document.getElementById('color-block2');
     const buttonCor4 = document.getElementById('color-block3');
     const painel = document.getElementById('pixel-board');
+    let corAtual = 0;
 
     IniciaPaleta();
     IniciaPainel(5, 5);
+
+    buttonCor1.addEventListener('click', function() 
+    {
+        TrocaCor(event, 0);
+    })
+    buttonCor2.addEventListener('click', function() 
+    {
+        TrocaCor(event, 1);
+    })
+    buttonCor3.addEventListener('click', function() 
+    {
+        TrocaCor(event, 2);
+    })
+    buttonCor4.addEventListener('click', function() 
+    {
+        TrocaCor(event, 3);
+    })
 
     function IniciaPaleta() 
     {
@@ -22,15 +40,55 @@ window.onload = function ()
         painel.style.maxWidth = (44 * coluna) + 'px';
         for (let i = 0; i < linha; i++)
         {
-            //painel.appendChild(document.createElement('br'));
-            
             for (let y = 0; y < coluna; y++) 
             {
-                let ele = document.createElement('div');
-                ele.setAttribute('class', 'pixel');
-                ele.style.backgroundColor = 'white';
-                painel.appendChild(ele);
+                let elemento = document.createElement('div');
+                elemento.setAttribute('class', 'pixel');
+                elemento.style.backgroundColor = 'white';
+                painel.appendChild(elemento);
             }            
         }
+    }
+
+    function TrocaCor(event, corFutura)
+    {
+        if(corAtual === 0)
+        {
+            buttonCor1.classList.remove('selected');
+            corAtual = corFutura;
+        }
+        else if(corAtual === 1)
+        {
+            buttonCor2.classList.remove('selected');
+            corAtual = corFutura;
+        }
+        else if(corAtual === 2)
+        {
+            buttonCor3.classList.remove('selected');
+            corAtual = corFutura;
+        }
+        else if(corAtual === 3)
+        {
+            buttonCor4.classList.remove('selected');
+            corAtual = corFutura;
+        }
+
+        if(corFutura === 0)
+        {
+            buttonCor1.className = 'color selected';
+        }
+        else if(corFutura === 1)
+        {
+            buttonCor2.className = 'color selected';
+        }
+        else if(corFutura === 2)
+        {
+            buttonCor3.className = 'color selected';
+        }
+        else if(corFutura === 3)
+        {
+            buttonCor4.className = 'color selected';
+        }
+        console.log(corAtual);
     }
 }; 
