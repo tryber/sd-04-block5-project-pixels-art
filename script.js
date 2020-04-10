642d64d3be0b19387fac5e268846d6af3a638298
AliceAAtalla <aliceatalla36@gmail.com>
2020-04-10 18:48:47 -0300

    Add palette colors

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-window.onload = function() {
-  loadBlackColor,
-}
+let colors;
+
+colors = document.getElementsByClassName("color");
+
+colors[0].style.backgroundColor = "black";
+colors[1].style.backgroundColor = "red";
+colors[2].style.backgroundColor = "yellow";
+colors[3].style.backgroundColor = "blue";
