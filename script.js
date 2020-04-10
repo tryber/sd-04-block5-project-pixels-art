0b8a2a7a63a41f845001f43c6584970afbe6f672
danilossousa <danilosantossousa@gmail.com>
2020-04-10 14:22:23 -0300

    v0.12.0.5

--- script.js
+++ script.js
@@ -10,14 +10,27 @@ const butClean = document.getElementById('clear-board');
 const tabCustom = document.getElementById('pixel-board-custom');
 const butCustom = document.getElementById('generate-board');
 
-// console.log(inpValue);
-
 function colorization() {
   colBlack.style.backgroundColor = colBlack.id;
   colRed.style.backgroundColor = colRed.id;
   colBlue.style.backgroundColor = colBlue.id;
   colGreen.style.backgroundColor = colGreen.id;
 }
+
+function insertPixels(v) {
+  for (i = 1; i <= v; i += 1) {
+    const elmTr = document.createElement('tr');
+    for (j = 1; j <= v; j += 1) {
+      const elmTd = document.createElement('td');
+      elmTd.className = 'pixel-custom';
+      elmTr.appendChild(elmTd);
+      tabCustom.appendChild(elmTr);
+    }
+  }
+}
+
+// -----------------
+// Funções de eventos
 
 function colSelected(e) {
   for (i = 0; i < colGroup.length; i += 1) {
@@ -37,32 +50,27 @@ function selPalette(e) {
   e.target.className = 'color selected';
 }
 
-function createPixels() {
-  let inpValue = document.getElementById('board-size').value;
-  while (tabCustom.firstChild) {
-    tabCustom.removeChild(tabCustom.firstChild);
-  }
-  if (inpValue < 5) {
-    inpValue = 5;
-  } else if (inpValue > 50) {
-    inpValue = 50;
-  }
-  for (i = 1; i <= inpValue; i += 1) {
-    const elmTr = document.createElement('tr');
-    for (j = 1; j <= inpValue; j += 1) {
-      const elmTd = document.createElement('td');
-      elmTd.className = 'pixel-custom';
-      elmTr.appendChild(elmTd);
-      tabCustom.appendChild(elmTr);
-    }
-  }
-}
-
 function cleanPainting() {
   for (i = 0; i < pixGroup.length; i += 1) {
     pixGroup[i].style.backgroundColor = 'white';
   }
 }
+
+function createPixels() {
+  let inpValue = document.getElementById('board-size').value;
+
+  while (tabCustom.firstChild) {
+    tabCustom.removeChild(tabCustom.firstChild);
+  }
+
+  if (inpValue < 5) { inpValue = 5; }
+  if (inpValue > 50) { inpValue = 50; }
+
+  insertPixels(inpValue);
+}
+
+// -----------------
+// Adição de eventos
 
 for (i = 0; i < pixGroup.length; i += 1) {
   pixGroup[i].addEventListener('click', colSelected);
