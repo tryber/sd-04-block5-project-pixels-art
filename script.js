2a39ad1bcdb74110832c8d07353da7abc3a809ca
Gabriel Almeida <gabriellukke@pop-os.localdomain>
2020-04-10 00:35:59 -0300

    added the clear board function

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ const colors = document.getElementsByClassName('color');
 const pixels = document.getElementsByClassName('pixel');
 
 pixels[1].style["background-color"] = "red";
+
+//Clear button
+const clearButton = document.getElementsByClassName('clear-btn');
+clearButton[0].addEventListener('click', function() {
+  for (pixel in pixels){
+  pixels[pixel].style.backgroundColor = "white";
+  }
+})
