85265a6f28caedfecc5691b4fab437f846e2ab90
Fanderson Kaecio <kaecio@yahoo.com.br>
2020-04-10 16:55:43 -0300

    add a cor preta como primeira cor selecionada

--- script.js
+++ script.js
@@ -1,17 +1,58 @@
 window.onload = function () {
   if (typeof Storage !== "undefined") {
+    // variaveis globais de cores
+    var blackColor = document.getElementById("color1");
+    blackColor.style.backgroundColor = "black";
 
-    let blackColor = document.getElementById("color1");
-    let redColor = document.getElementById("color2");
-    let bluekColor = document.getElementById("color3");
-    let greenColor = document.getElementById("color4");
-    let pixel = document.getElementsByClassName("pixel");
+    var redColor = document.getElementById("color2");
+    redColor.style.backgroundColor = "red";
 
-    function mudacor() {
-        pixel.color = "black";
+    var blueColor = document.getElementById("color3");
+    blueColor.style.backgroundColor = "blue";
+
+    var greenColor = document.getElementById("color4");
+    greenColor.style.backgroundColor = "green";
+
+    // variavel guarda a div com a classe selected
+    var pixelColorful = document.querySelector('.selected');
+
+    blackColor.addEventListener("click", function () {
+      pixelColorful.classList.remove("selected");
+      blackColor.classList.add("selected");
+      console.log("black");
+    });
+
+    redColor.addEventListener("click", function () {
+      pixelColorful.classList.remove("selected");
+      redColor.classList.add("selected");
+      console.log("red");
+    });
+
+    blueColor.addEventListener("click", function () {
+      pixelColorful.classList.remove("selected");
+      blueColor.classList.add("selected");
+      console.log("Blue");
+    });
+
+    greenColor.addEventListener("click", function () {
+      pixelColorful.classList.remove("selected");
+      greenColor.classList.add("selected");
+      console.log("green");
+    });
+
+    //variavel guarda as divs com a classe pixel
+    let squareSelect = document.querySelectorAll(".pixel");
+
+    function fillPixel(i) {
+      squareSelect[i].addEventListener("click", function () {
+        let colorSelected = pixelColorful;
+        squareSelect[i].style.backgroundColor =
+          colorSelected.style.backgroundColor;
+      });
     }
-    blackColor.addEventListener("click", mudacor);
-    
+    for (let i = 0; i < squareSelect.length; i++) {
+      fillPixel(i);
+    }
   } else {
     console.log("Sem suporte para Web Storage");
   }
