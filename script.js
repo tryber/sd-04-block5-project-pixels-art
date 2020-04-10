809357d13d6352966dab27843bb14cafceb52700
Fanderson Kaecio <kaecio@yahoo.com.br>
2020-04-09 23:44:40 -0300

    formatando o css

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 window.onload = function () {
   if (typeof Storage !== "undefined") {
+
+    let blackColor = document.getElementById("color1");
+    let redColor = document.getElementById("color2");
+    let bluekColor = document.getElementById("color3");
+    let greenColor = document.getElementById("color4");
+    let pixel = document.getElementsByClassName("pixel");
+
+    function mudacor() {
+        pixel.color = "black";
+    }
+    blackColor.addEventListener("click", mudacor);
+    
   } else {
     console.log("Sem suporte para Web Storage");
   }
