e1415fc6df77eaf339d586f074025d998eb9dd84
Alexandre Anicio <alexandre.anicio94@gmail.com>
2020-04-10 14:06:39 -0300

    refactoring

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-window.onload = function () {
-  const color1 = document.getElementById("color1");
-  const color2 = document.getElementById("color2");
-  const color3 = document.getElementById("color3");
-  const color4 = document.getElementById("color4");
+window.onload = () => {
 
-  color1.addEventListener('click', function () {
-    document.querySelector('.selected').classList.remove('selected');
-    color1.classList.add('selected');
-  });
-  color2.addEventListener('click', function () {
-    document.querySelector('.selected').classList.remove('selected');
-    color2.classList.add('selected');
-    console.log(document.querySelector('.selected'));
-  });
-  color3.addEventListener('click', function () {
-    document.querySelector('.selected').classList.remove('selected');
-    color3.classList.add('selected');
-  });
-  color4.addEventListener('click', function () {
-    document.querySelector('.selected').classList.remove('selected');
-    color4.classList.add('selected');
+  for (let i = 1; i <= 4; i += 1) {
+    document.getElementById(`color${i}`).addEventListener('click', function () {
+      document.querySelector('.selected').classList.remove('selected');
+      document.getElementById(`color${i}`).classList.add('selected');
+    });
+  }
+
+  let pixels = document.getElementById('pixel-board');
+  pixels.addEventListener('click', function (event) {
+    const selected = document.querySelector('.selected');
+    event.target.style.backgroundColor = selected.style.backgroundColor;
   });
 
+  let clear = document.getElementById('clear-board');
+  let pixels2 = document.querySelectorAll('.pixel');
+  console.log(pixels2);
+
+  clear.addEventListener('click', function () {
+    for (let i = 0; i < pixels2.length; i += 1) {
+      pixels2[i].style.backgroundColor = 'white';
+    }
+  });
 }
