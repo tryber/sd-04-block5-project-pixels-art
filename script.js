4f87a514e64312edc8bc57336919e3c502b39be0
Lucas Barbosa Rosa <lucaslucas2008@gmail.com>
2020-04-10 18:36:35 -0300

    Add requisition 4-6

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ window.onload = function ()
     const buttonCor2 = document.getElementById('color-block1');
     const buttonCor3 = document.getElementById('color-block2');
     const buttonCor4 = document.getElementById('color-block3');
+    const painel = document.getElementById('pixel-board');
 
     IniciaPaleta();
+    IniciaPainel(5, 5);
 
     function IniciaPaleta() 
     {
@@ -13,5 +15,23 @@ window.onload = function ()
         buttonCor2.style.backgroundColor = 'rgb(255,0,0)';
         buttonCor3.style.backgroundColor = 'rgb(0,255,0)';
         buttonCor4.style.backgroundColor = 'rgb(0,0,255)';
+    }
+
+    function IniciaPainel(linha, coluna)
+    {
+        painel.style.maxWidth = (44 * coluna) + 'px';
+        for (let i = 0; i < linha; i++)
+        {
+            //painel.appendChild(document.createElement('br'));
+            
+            for (let y = 0; y < coluna; y++) 
+            {
+                let ele = document.createElement('div');
+                ele.setAttribute('class', 'pixel');
+                console.log(ele);
+                console.log(painel);
+                painel.appendChild(ele);
+            }            
+        }
     }
 }; 
