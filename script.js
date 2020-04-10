17399fe45a43c88648ddc295436ad7f1a13009d6
Fanderson Kaecio <kaecio@yahoo.com.br>
2020-04-10 18:19:20 -0300

    addedr as cores pedidas

--- script.js
+++ script.js
@@ -1,59 +1,53 @@
 window.onload = function () {
-  if (typeof Storage !== "undefined") {
-    // variaveis globais de cores
-    var blackColor = document.getElementById("color1");
-    blackColor.style.backgroundColor = "black";
+  // variaveis globais de cores
+  let blackColor = document.querySelector(".selected");
+  blackColor.style.backgroundColor = "black";
 
-    var redColor = document.getElementById("color2");
-    redColor.style.backgroundColor = "red";
+  let redColor = document.getElementById("color2");
+  redColor.style.backgroundColor = "red";
 
-    var blueColor = document.getElementById("color3");
-    blueColor.style.backgroundColor = "blue";
+  let blueColor = document.getElementById("color3");
+  blueColor.style.backgroundColor = "blue";
 
-    var greenColor = document.getElementById("color4");
-    greenColor.style.backgroundColor = "green";
+  let greenColor = document.getElementById("color4");
+  greenColor.style.backgroundColor = "green";
 
-    // variavel guarda a div com a classe selected
-    var pixelColorful = document.querySelector('.selected');
 
-    blackColor.addEventListener("click", function () {
-      pixelColorful.classList.remove("selected");
-      blackColor.classList.add("selected");
-      console.log("black");
+  blackColor.addEventListener("click", function () {
+    document.querySelector(".selected").classList.remove("selected");
+    blackColor.classList.add("selected");
+    console.log(document.querySelector('.selected').style.backgroundColor);
+  });
+
+  redColor.addEventListener("click", function () {
+    document.querySelector(".selected").classList.remove("selected");
+    redColor.classList.add("selected");
+    console.log(document.querySelector('.selected').style.backgroundColor);
+  });
+
+  blueColor.addEventListener("click", function () {
+    document.querySelector('.selected').classList.remove("selected");
+    blueColor.classList.add("selected");
+    console.log(document.querySelector('.selected').style.backgroundColor);
+  });
+
+  greenColor.addEventListener("click", function () {
+    document.querySelector('.selected').classList.remove("selected");
+    greenColor.classList.add("selected");
+    console.log(document.querySelector('.selected').style.backgroundColor);
+  });
+
+  //variavel guarda as divs com a classe pixel
+  let squareSelect = document.querySelectorAll(".pixel");
+
+  function fillPixel(i) {
+    squareSelect[i].addEventListener("click", function () {
+      let colorSelected = document.querySelector('.selected');
+      squareSelect[i].style.backgroundColor =
+        colorSelected.style.backgroundColor;
     });
-
-    redColor.addEventListener("click", function () {
-      pixelColorful.classList.remove("selected");
-      redColor.classList.add("selected");
-      console.log("red");
-    });
-
-    blueColor.addEventListener("click", function () {
-      pixelColorful.classList.remove("selected");
-      blueColor.classList.add("selected");
-      console.log("Blue");
-    });
-
-    greenColor.addEventListener("click", function () {
-      pixelColorful.classList.remove("selected");
-      greenColor.classList.add("selected");
-      console.log("green");
-    });
-
-    //variavel guarda as divs com a classe pixel
-    let squareSelect = document.querySelectorAll(".pixel");
-
-    function fillPixel(i) {
-      squareSelect[i].addEventListener("click", function () {
-        let colorSelected = pixelColorful;
-        squareSelect[i].style.backgroundColor =
-          colorSelected.style.backgroundColor;
-      });
-    }
-    for (let i = 0; i < squareSelect.length; i++) {
-      fillPixel(i);
-    }
-  } else {
-    console.log("Sem suporte para Web Storage");
+  }
+  for (let i = 0; i < squareSelect.length; i++) {
+    fillPixel(i);
   }
 };
