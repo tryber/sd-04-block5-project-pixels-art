2a76e1b9aee16eadc0c859276797c6bd847773cf
frederico-campello <fredfmc33@gmail.com>
2020-04-11 16:30:47 -0300

    Iniciando o projeto

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 window.onload = function () {
-  const corInicial = document.querySelector('.color');
-  corInicial.className = 'selected';
-};
+  let paletaDeCores = document.getElementById('color-palette');
+  let quadroDePixels = document.getElementById('pixel-board');
+
+  function alteraClasse (event) {
+  //console.log(event.target.className);
+  document.querySelector('.selected').classList.remove('selected');
+  event.target.classList.add('selected');
+  }
+  paletaDeCores.addEventListener('click', alteraClasse);
+
+  function colorePixel (event)  {
+  //console.log(event.target);
+  let corSelecionada = document.querySelector('.selected').style.backgroundColor;
+  event.target.style.backgroundColor = corSelecionada;
+  }
+  quadroDePixels.addEventListener('click', colorePixel);
+
+
+}
