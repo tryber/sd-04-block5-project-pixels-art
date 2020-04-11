fffe4df8f0012f0b06a050b079768dc39820c62e
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2020-04-11 06:38:45 -0300

    mudança na construção dos pixels visando cumprir criterios

--- script.js
+++ script.js
@@ -5,34 +5,22 @@ window.onload = function() {
 
 function createPixelBoard() {
   let pixelBoard = document.getElementById("pixel-board");
-  for(let i=0; i<4; i+=1){
-    pixelBoard.appendChild(createPixelBoardRow());
+  let finish = 0
+  if(finish<4){
+    for(let i=0; i<5; i+=1){
+      let row = document.createElement("div");
+      pixelBoard.appendChild(row);
+      }
+      pixelBoard.appendChild("br")
+      finish += 1;
+  } else {
+    for(let i=0; i<5; i+=1){
+      let row = document.createElement("div");
+      pixelBoard.appendChild(row);
+      }
   }
-  pixelBoard.appendChild(createLastPixelRow());
 }
 
-function createPixelBoardRow() {
-  let a = document.createElement("div");
-  a.setAttribute("class", "row")
-  for(let i=0; i<5; i+=1){
-    let colorDiv = document.createElement("div");
-    colorDiv.setAttribute("class", "pixel")
-    a.appendChild(colorDiv);
-  }
-    a.setAttribute("class", "row");
-  return a;
-}
-
-function createLastPixelRow() {
-  let a = document.createElement("div");
-  a.setAttribute("class", "lastRow")
-  for(let i=0; i<5; i+=1){
-    let colorDiv = document.createElement("div");
-    colorDiv.setAttribute("class", "pixel");
-    a.appendChild(colorDiv);
-  }
-  return a; 
-}
 
 function createPalette() {
   let colorPalette = document.getElementById("color-palette");
