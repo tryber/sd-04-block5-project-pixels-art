9929eb5c9b8597509c233bc2aa355416efbaecb9
igorgbr <igorgphotoarte@gmail.com>
2020-04-11 17:17:40 -0300

    criando Funçoes - melhorando codigo 2 -

--- script.js
+++ script.js
@@ -4,11 +4,6 @@ colorRed = document.querySelector("#color-red");
 colorBlue = document.querySelector("#color-blue");
 colorGreen = document.querySelector("#color-green");
 
-let var1 = colorGreen.style.backgroundColor = 'green';
-let var2 = colorRed.style.backgroundColor = 'red';
-let var3 = colorBlue.style.backgroundColor = 'blue';
-let var4 = colorBlack.style.backgroundColor = 'black';
-
 pixelBoard = document.querySelector("#pixel-board")
 pixel = document.querySelectorAll(".pixel");
 
@@ -18,7 +13,6 @@ colorRed.className = 'color';
 colorBlue.className = 'color'; 
 colorGreen.className = 'color'; 
 };
-
 colorBlack.addEventListener('click', function(){ 
     apagaClass();
     colorBlack.className = 'color selected';
@@ -41,6 +35,10 @@ colorGreen.addEventListener('click', function(){
 
     pixelBoard.addEventListener('click', function (event) {
     const selectedColor = document.querySelector('.selected').style.backgroundColor;
+    console.log(selectedColor)
+    if(selectedColor == null){
+      event.target.style.backgroundColor = 'black';
+    }
      event.target.style.backgroundColor = selectedColor;
     });
 
@@ -48,6 +46,7 @@ colorGreen.addEventListener('click', function(){
 let botaoLimpa = document.querySelector('#clear-board');
 botaoLimpa.addEventListener('click', function(){
  for (i=0; i < pixel.length; i++) {
+   pixel = document.querySelectorAll(".pixel");
     pixel[i].style.backgroundColor = 'white';
  }})
 
@@ -55,61 +54,55 @@ botaoLimpa.addEventListener('click', function(){
     // const corAleatoria1 = arrayCores[Math.floor(Math.random() * arrayCores.length)];
     // const randoColorTwo = arrayCores[Math.floor(Math.random() * arrayCores.length)];
     // const randoColorGreen = arrayCores[Math.floor(Math.random() * arrayCores.length)];
+    const colorBlack = document.querySelector("#color-black")
     const randoColorRed = Number.parseInt(Math.random() * 255, 10);
     const randoColorBlue = Number.parseInt(Math.random() * 255, 10);
     const randoColorGreen = Number.parseInt(Math.random() * 255, 10);
+    colorBlack.style.backgroundColor = 'black';
     colorRed.style.backgroundColor = `rgb(${randoColorGreen},${randoColorRed},${randoColorBlue})`;
     colorBlue.style.backgroundColor = `rgb(${randoColorBlue},${randoColorRed},${randoColorGreen})`;
     colorGreen.style.backgroundColor = `rgb(${randoColorRed},${randoColorGreen},${randoColorRed})`;
  };
 
 //BONUS 2
- let arrayCores = [var1, var2, var3, var4];
- for (i = 1; i < arrayCores.length; i++){
+ for (i = 1; i < 3; i++){
     sortColor()
  };
+ let btnGenerateBoard = document.querySelector('#generate-board'); //botao adiciona quadrado
+ let inputBoard = document.querySelector('#board-size'); //caixa com valor
 
- let btnGenerateBoard = document.querySelector('#generate-board');
- let inputBoard = document.querySelector('#board-size');
+ function divSize(qtdQuadrados){
+   let qtdQuadrad1 = qtdQuadrados * 40;
+   let qtdQuadrad2 =  qtdQuadrados * 2;
+   const ladoQuadrado = qtdQuadrad1 + qtdQuadrad2;
 
-
- function deletePixel(){
-
+   pixelBoard.style.height = `${ladoQuadrado}px`;
+   pixelBoard.style.width = `${ladoQuadrado}px`;
  }
-
- function deletePixelGerado(divPixel){
-    for(i = 0; i < divPixel.length; i++){
-       pixelBoard.removeChild(divPixel[i])
-    }
-}
-
+ function limpaPixel(){
+   for(i = 0; i < pixel.length; i++){
+      pixelBoard.removeChild(pixel[i])
+   }
+ }
+ function criaPixels (qtdQuadrados) {
+   for(i=0;i < (qtdQuadrados*qtdQuadrados); i++){
+      let pixel = document.createElement('div');
+      pixel.setAttribute('class', 'pixel')
+      pixelBoard.appendChild(pixel)
+   }   
+ }
  btnGenerateBoard.addEventListener('click', function(){
-    console.log(pixel)
     pixel = document.querySelectorAll(".pixel");
-    for(i = 0; i < pixel.length; i++){
-       pixelBoard.removeChild(pixel[i])
-    }
+      limpaPixel(); //Limpa Pixels anteriores
      let qtdQuadrados = inputBoard.value;
 
+     //controle de quantidade de quadrados
      if (qtdQuadrados < 5) {
-        qtdQuadrados = 5;
-      } else if (qtdQuadrados > 50) {
-        qtdQuadrados = 50;
-      }
-     
-     let qtdQuadrad1 = qtdQuadrados * 40;
-     let qtdQuadrad2 =  qtdQuadrados * 2;
-    
-     const ladoQuadrado = qtdQuadrad1 + qtdQuadrad2;
- 
+      qtdQuadrados = 5;
+    } else if (qtdQuadrados > 50) {
+      qtdQuadrados = 50;
+    }
 
-     pixelBoard.style.height = `${ladoQuadrado}px`;
-     pixelBoard.style.width = `${ladoQuadrado}px`;
-
-     for(i=0;i < (qtdQuadrados*qtdQuadrados); i++){
-        let pixel = document.createElement('div');
-        pixel.setAttribute('class', 'pixel')
-        pixelBoard.appendChild(pixel)
-     }   
-
+      divSize(qtdQuadrados); //Tamanho do DIV ao add Pixels
+      criaPixels(qtdQuadrados) //Add Pixels
  })
