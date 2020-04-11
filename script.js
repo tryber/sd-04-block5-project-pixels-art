20daa497570f267a6ee79269cf5638e04f55bfe4
frederico-campello <fredfmc33@gmail.com>
2020-04-11 16:36:42 -0300

    Atualizando o projeto

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
 window.onload = function () {
-  let paletaDeCores = document.getElementById('color-palette');
-  let quadroDePixels = document.getElementById('pixel-board');
-
-  function alteraClasse (event) {
-  //console.log(event.target.className);
-  document.querySelector('.selected').classList.remove('selected');
-  event.target.classList.add('selected');
+  const paletaDeCores = document.getElementById('color-palette');
+  const quadroDePixels = document.getElementById('pixel-board');
+  //
+  function alteraClasse(event) {
+  // console.log(event.target.className);
+    document.querySelector('.selected').classList.remove('selected');
+    event.target.classList.add('selected');
   }
   paletaDeCores.addEventListener('click', alteraClasse);
-
-  function colorePixel (event)  {
-  //console.log(event.target);
-  let corSelecionada = document.querySelector('.selected').style.backgroundColor;
-  event.target.style.backgroundColor = corSelecionada;
+  //
+  function colorePixel(event) {
+  // console.log(event.target);
+    const corSelecionada = document.querySelector('.selected').style.backgroundColor;
+    event.target.style.backgroundColor = corSelecionada;
   }
   quadroDePixels.addEventListener('click', colorePixel);
-
-
-}
+};
