03a061dd1b5e86c1d633503ef89d03fc4b1082f6
jessica-tavares <jessicatavareseel@gmail.com>
2020-04-12 02:30:32 -0300

    mudando a class selected

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const purple = document.getElementsByClassName('color')[1];
 const pink = document.getElementsByClassName('color')[2];
 const yellow = document.getElementsByClassName('color')[3];
 const clearButton = document.getElementById('clear-board');
+const cores = document.getElementById('color-palette').childNodes
 const pixel = [];
 for (let i = 0; i < 25; i += 1) {
   pixel[i] = document.getElementsByClassName('pixel')[i];
@@ -11,7 +12,7 @@ for (let i = 0; i < 25; i += 1) {
 
 
 // Funçoes
-function saveColor(n) {
+function saveColor(n, c) {
   const color = n;
   localStorage.removeItem('color');
   localStorage.setItem('color', color);
@@ -28,26 +29,38 @@ function changeColor(j) {
 }
 
 window.onload = function () {
-  saveColor('black');
+  saveColor('black', 'black');
   black.className = 'color black selected';
 };
 
 // Criando os event Listener
 black.addEventListener('click', function () {
-  saveColor('black');
+  saveColor('black', 'black');
   black.className = 'color black selected';
+  purple.className = 'color purple';
+  pink.className = 'color pink';
+  yellow.className = 'color yellow';
 });
 purple.addEventListener('click', function () {
-  saveColor('blueviolet');
+  saveColor('blueviolet', 'purple');
   purple.className = 'color purple selected';
+  black.className = 'color black';
+  pink.className = 'color pink';
+  yellow.className = 'color yellow';
 });
 pink.addEventListener('click', function () {
-  saveColor('rgb(230, 47, 160)');
+  saveColor('rgb(230, 47, 160)', 'pink');
   pink.className = 'color pink selected';
+  black.className = 'color black';
+  purple.className = 'color purple';
+  yellow.className = 'color yellow';
 });
 yellow.addEventListener('click', function () {
-  saveColor('yellow');
+  saveColor('yellow', 'yellow');
   yellow.className = ' color yellow selected';
+  black.className = 'color black';
+  pink.className = 'color pink';
+  purple.className = 'color purple';
 });
 clearButton.addEventListener('click', clearBoard);
 
