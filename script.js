6dbf94dcb29223cf2503624a514ff7c6ddbaec46
Gabriel Almeida <gabriellukke@pop-os.localdomain>
2020-04-13 18:46:12 -0300

    Clear board function added and styled

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const palette = document.getElementById('color-palette');
 const pixelBoard = document.getElementById('pixel-board');
 const colors = document.getElementsByClassName('color');
 const pixels = document.getElementsByClassName('pixel');
-const selectedColor = document.querySelector('.selected');
+const clearBtn = document.getElementById('clear-board');
 
 // Add and Remove the Class "Selected"
 palette.addEventListener('click', function(event) {
@@ -13,4 +13,11 @@ palette.addEventListener('click', function(event) {
 // Color the Pixel Board
 pixelBoard.addEventListener('click', function(event) {
   event.target.style.backgroundColor = document.querySelector('.selected').id;
+})
+
+// Clear Board
+clearBtn.addEventListener('click', function() {
+  for (pixel in pixels) {
+    pixels[pixel].style.backgroundColor = "white";
+  }
 })
