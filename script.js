658bc7f55201c5a0629e0a4c2ded5dbee5f84d1e
Pedro Pires <pedrotpoliveira@gmail.com>
2020-04-13 19:01:37 -0300

    Commit 6 - Finer mods

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
-const colors = document.getElementsByClassName('color');
-let selectedColor = 'black';
-function checkColor() {
-  for (let i = 0; i < 4; i += 1) {
-    colors[i].classList.remove('selected');
-  }
-  this.classList.add('selected');
-  selectedColor = this.classList.item(1);
-}
-for (let i = 0; i < 4; i += 1) {
-  colors[i].addEventListener('click', checkColor);
-}
+window.onload = function () {
+  const colors = document.getElementsByClassName('color');
+  const pixels = document.getElementsByClassName('pixel');
+  let selectedColor = 'black';
 
-const pixels = document.getElementsByClassName('pixel');
-function checkPixel() {
-  if (this.classList.length === 2) {
-    this.classList.remove(this.classList.item(1));
-    this.classList.add(selectedColor);
-  } else {
-    this.classList.add(selectedColor);
+  function checkColor() {
+    for (let i = 0; i < 4; i += 1) {
+      colors[i].classList.remove('selected');
+    }
+    this.classList.add('selected');
+    selectedColor = this.classList.item(1);
   }
-}
-for (let i = 0; i < 25; i += 1) {
-  pixels[i].addEventListener('click', checkPixel);
-}
+
+  function checkPixel() {
+    if (this.classList.length === 2) {
+      this.classList.remove(this.classList.item(1));
+      this.classList.add(selectedColor);
+    } else {
+      this.classList.add(selectedColor);
+    }
+  }
+
+  for (let i = 0; i < 4; i += 1) {
+    colors[i].addEventListener('click', checkColor);
+  }
+  for (let i = 0; i < 25; i += 1) {
+    pixels[i].addEventListener('click', checkPixel);
+  }
+};
