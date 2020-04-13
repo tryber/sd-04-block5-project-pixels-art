8ecc6653c28ad8ad2f35e7245db09c3cc584a060
binhus <eng.robsonwilli@gmail.com>
2020-04-13 19:16:35 -0300

    fixing codeclimate issues, color selected black as default, put only 4 colors

--- script.js
+++ script.js
@@ -1,29 +1,22 @@
-let black = document.getElementById("black");
-let red = document.getElementById("red");
-let blue = document.getElementById("blue");
-let yellow = document.getElementById("yellow");
-let green = document.getElementById("green");
-let orange = document.getElementById("orange");
-let white = document.getElementById("white");
-let selected = ""
-let pixelBoard = document.getElementById("pixel-board");
-let colorPalette = document.getElementById("color-palette");
+const black = document.getElementById('black');
+const red = document.getElementById('red');
+const blue = document.getElementById('blue');
+const yellow = document.getElementById('yellow');
+let selected = 'black';
+const pixelBoard = document.getElementById('pixel-board');
+const colorPalette = document.getElementById('color-palette');
 
-black.style.backgroundColor = "black";
-red.style.backgroundColor = "red";
-blue.style.backgroundColor = "blue";
-yellow.style.backgroundColor = "yellow";
-green.style.backgroundColor = "green";
-orange.style.backgroundColor = "orange";
-white.style.backgroundColor = "white";
 
-colorPalette.addEventListener("click",function(event){
-    selected = event.target.id;
-    event.className = "selected"
+black.style.backgroundColor = 'black';
+red.style.backgroundColor = 'red';
+blue.style.backgroundColor = 'blue';
+yellow.style.backgroundColor = 'yellow';
+
+colorPalette.addEventListener('click', function (event) {
+  selected = event.target.id;
 });
 
-pixelBoard.addEventListener("click", function(event){
-    var targetElement = event.target;
-    targetElement.style.backgroundColor = selected;
-    console.log(targetElement);
-})
+pixelBoard.addEventListener('click', function (event) {
+  const targetElement = event.target;
+  targetElement.style.backgroundColor = selected;
+});
