7391cf2e644590b3b3d21b66c84246709f914c92
Lucas Barbosa Rosa <lucaslucas2008@gmail.com>
2020-04-12 21:16:30 -0300

    Changes to code climate 1.4

--- script.js
+++ script.js
@@ -11,6 +11,36 @@ window.onload = function () {
   let coLinAtual = 5;
   let coLinFutura = 0;
 
+  function SorteiaCor(min, max) {
+    let cor = 'rgb(';
+    for (let i = 0; i < 3; i++) {
+      if (i != 2) {
+        cor += (Math.floor(Math.random() * (max - min)) + min) + ',';
+      }
+      else {
+        cor += (Math.floor(Math.random() * (max - min)) + min) + ')';
+      }
+    }
+    return cor;
+  }
+
+  function Pintar(event) {
+    let cor;
+    if (corAtual === 0) {
+      cor = buttonCor1.style.backgroundColor;
+    }
+    else if (corAtual === 1) {
+      cor = buttonCor2.style.backgroundColor;
+    }
+    else if (corAtual === 2) {
+      cor = buttonCor3.style.backgroundColor;
+    }
+    else if (corAtual === 3) {
+      cor = buttonCor4.style.backgroundColor;
+    }
+    event.target.style.backgroundColor = cor;
+  }
+
   function IniciaPaleta() {
     buttonCor1.style.backgroundColor = 'black';
     buttonCor2.style.backgroundColor = SorteiaCor(0, 255);
@@ -19,106 +49,77 @@ window.onload = function () {
   }
 
   function IniciaPainel(linha, coluna) {
-    painel.style.maxWidth = (44 * coluna) + 'px';
-    for (let i = 0; i < linha; i++) {
-        for (let y = 0; y < coluna; y++) {
-            let elemento = document.createElement('div');
-            elemento.setAttribute('class', 'pixel');
-            elemento.style.backgroundColor = 'white';
-            elemento.addEventListener('click', function (event) {
-                Pintar(event);
-            });
-            painel.appendChild(elemento);
-        }
+    let tamanho = (44 * coluna) + 'px';
+    painel.style.maxWidth = tamanho;
+    for (let i = 0; i < linha; i+1) {
+      for (let y = 0; y < coluna; y+1) {
+        let elemento = document.createElement('div');
+        elemento.setAttribute('class', 'pixel');
+        elemento.style.backgroundColor = 'white';
+        elemento.addEventListener('click', function (event) {
+            Pintar(event);
+        });
+        painel.appendChild(elemento);
+      }
     }
   }
 
   function TrocaCor(event, corFutura) {
     if (corAtual === 0) {
-        buttonCor1.classList.remove('selected');
-        corAtual = corFutura;
+      buttonCor1.classList.remove('selected');
+      corAtual = corFutura;
     }
     else if (corAtual === 1) {
-        buttonCor2.classList.remove('selected');
-        corAtual = corFutura;
+      buttonCor2.classList.remove('selected');
+      corAtual = corFutura;
     }
     else if (corAtual === 2) {
-        buttonCor3.classList.remove('selected');
-        corAtual = corFutura;
+      buttonCor3.classList.remove('selected');
+      corAtual = corFutura;
     }
     else if (corAtual === 3) {
-        buttonCor4.classList.remove('selected');
-        corAtual = corFutura;
+      buttonCor4.classList.remove('selected');
+      corAtual = corFutura;
     }
 
     if (corFutura === 0) {
-        buttonCor1.className = 'color selected';
+      buttonCor1.className = 'color selected';
     }
     else if (corFutura === 1) {
-        buttonCor2.className = 'color selected';
+      buttonCor2.className = 'color selected';
     }
     else if (corFutura === 2) {
-        buttonCor3.className = 'color selected';
+      buttonCor3.className = 'color selected';
     }
     else if (corFutura === 3) {
-        buttonCor4.className = 'color selected';
+      buttonCor4.className = 'color selected';
     }
-  }
-
-  function Pintar(event) {
-    let cor;
-    if (corAtual === 0) {
-        cor = buttonCor1.style.backgroundColor;
-    }
-    else if (corAtual === 1) {
-        cor = buttonCor2.style.backgroundColor;
-    }
-    else if (corAtual === 2) {
-        cor = buttonCor3.style.backgroundColor;
-    }
-    else if (corAtual === 3) {
-        cor = buttonCor4.style.backgroundColor;
-    }
-    event.target.style.backgroundColor = cor;
   }
 
   function LimpaTudo() {
     for (let i = 0; i < (coLinAtual * coLinAtual); i++) {
-        painel.childNodes[i].style.backgroundColor = 'white';
+      painel.childNodes[i].style.backgroundColor = 'white';
     }
-  }
-
-  function SorteiaCor(min, max) {
-    let cor = 'rgb(';
-    for (let i = 0; i < 3; i++) {
-        if (i != 2) {
-            cor += (Math.floor(Math.random() * (max - min)) + min) + ',';
-        }
-        else {
-            cor += (Math.floor(Math.random() * (max - min)) + min) + ')';
-        }
-    }
-    return cor;
   }
 
   function TiraPainel(elemento) {
     if (elemento >= 0) {
-        for (let i = 0; i < coLinAtual; i++) {
-            for (let y = 0; y < coLinAtual; y++) {
-                painel.removeChild(painel.firstChild);
-            }
+      for (let i = 0; i < coLinAtual; i++) {
+        for (let y = 0; y < coLinAtual; y++) {
+            painel.removeChild(painel.firstChild);
         }
-        if (elemento < 5) {
-            coLinFutura = 5;
-        }
-        else if (elemento > 50) {
-            coLinFutura = 50;
-        }
-        else {
-            coLinFutura = elemento;
-        }
-        IniciaPainel(coLinFutura, coLinFutura);
-        coLinAtual = coLinFutura;
+      }
+      if (elemento < 5) {
+        coLinFutura = 5;
+      }
+      else if (elemento > 50) {
+        coLinFutura = 50;
+      }
+      else {
+        coLinFutura = elemento;
+      }
+      IniciaPainel(coLinFutura, coLinFutura);
+      coLinAtual = coLinFutura;
     }
   }
 
