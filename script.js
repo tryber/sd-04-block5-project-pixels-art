92bcd6bdf314af6420fad868b2f75f2f6ed7d66c
Gabriel Almeida <gabriellukke@pop-os.localdomain>
2020-04-13 18:51:09 -0300

    Codeclimate Adjusts

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
 const palette = document.getElementById('color-palette');
 const pixelBoard = document.getElementById('pixel-board');
-const colors = document.getElementsByClassName('color');
 const pixels = document.getElementsByClassName('pixel');
 const clearBtn = document.getElementById('clear-board');
 
-// Add and Remove the Class "Selected"
-palette.addEventListener('click', function(event) {
+// Select Color
+palette.addEventListener('click', function (event) {
   document.querySelector('.selected').classList.remove('selected');
   event.target.classList.add('selected');
 });
 
 // Color the Pixel Board
-pixelBoard.addEventListener('click', function(event) {
+pixelBoard.addEventListener('click', function (event) {
   event.target.style.backgroundColor = document.querySelector('.selected').id;
-})
+});
 
 // Clear Board
-clearBtn.addEventListener('click', function() {
-  for (pixel in pixels) {
+clearBtn.addEventListener('click', function () {
+  for (let pixel in pixels) {
     pixels[pixel].style.backgroundColor = "white";
   }
-})
+});
