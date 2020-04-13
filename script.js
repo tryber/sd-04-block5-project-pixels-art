7997082f9e05638aaf65de79534c138a6969c620
Victor L <victor.lustosacp@gmail.com>
2020-04-13 18:52:29 -0300

    testando troca de cor dos pixels

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 window.onload = function() {
     document.getElementById('black').classList.add( 'selected' ); 
-
-    console.log(document.getElementById('black').classList);
     let selected = document.getElementById('color-palette');
     let color = 'black';
-      selected.addEventListener("click",function(event){
-          color = String(event.target.id);
-          if(selected.classList ==! 'selected'){
-          event.target.classList.add('selected');
-          console.log(event.target.classList);
-          }
-      })
+    selected.addEventListener("click",function(event){
+        document.getElementById(color).classList.remove('selected');
+        color = String(event.target.id);
+        event.target.classList.add('selected');
+    })
+    
     let table = document.getElementById('pixel-board');
     table.addEventListener("click",function(event){
         event.target.id = color;
