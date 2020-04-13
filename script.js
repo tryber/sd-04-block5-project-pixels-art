214c26c4f5f288867589a759355665e65c585e38
Gabriel Almeida <gabriellukke@pop-os.localdomain>
2020-04-13 18:09:00 -0300

    Change the function to select the color and color the pixel board

--- script.js
+++ script.js
@@ -2,14 +2,15 @@ const palette = document.getElementById('color-palette');
 const pixelBoard = document.getElementById('pixel-board');
 const colors = document.getElementsByClassName('color');
 const pixels = document.getElementsByClassName('pixel');
-const selectedColor = document.getElementsByClassName('selected');
-const color = selectedColor.id;
+const selectedColor = document.querySelector('.selected');
 
+// Add and Remove the Class "Selected"
 palette.addEventListener('click', function(event) {
-  for(c in colors) {
-    colors[c].classList.remove('selected');
-    event.target.classList.add('selected');
-  }
+  selectedColor.classList.remove('selected');
+  event.target.classList.add('selected');
+});
+
+// Color the Pixel Board
+pixelBoard.addEventListener('click', function(event) {
+  event.target.style.backgroundColor = document.querySelector('.selected').id;
 })
-
-
