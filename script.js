aa90e3a8860b720d1a5b9213efb566ad1ffcc538
ederusena <eder.sena@live.com>
2020-04-13 16:54:06 -0300

    Fix codeclimate errors

--- script.js
+++ script.js
@@ -6,36 +6,42 @@ function randomColors() {
     arrayColors[i] = `#${randomColor}`;
   }
 }
-window.onload = () => {
-  randomColors();
-  // variables
-  const elementsColor = document.getElementsByClassName('color');
-  const palette = document.getElementById('color-palette');
-  const pixelBoard = document.getElementById('pixel-board');
-  const clearBoard = document.getElementById('clear-board');
-  // paint the elements background
-  for (let i = 0; i < elementsColor.length; i += 1) {
-    elementsColor[i].style.backgroundColor = arrayColors[i];
-  }
-  // remove and add class selected
-  palette.addEventListener('click', (event) => {
-    document.querySelector('.selected').classList.remove('selected');
-    event.target.classList.add('selected');
-  });
-  // paint the pixel board with the color that have the selected class
-  pixelBoard.addEventListener('click', (event) => {
-    const selectedColor = document.querySelector('.selected');
-    event.target.style.backgroundColor = selectedColor.style.backgroundColor;
-  });
-  // clear the board
+
+// clear the board
+function clearBoard2(clearBoard) {
   clearBoard.addEventListener('click', () => {
     const pixels = document.querySelectorAll('.pixel');
     for (let i = 0; i < pixels.length; i += 1) {
       pixels[i].style.backgroundColor = 'white';
     }
   });
-  // Generate board
-  const generateBoard = document.getElementById('generate-board')
+}
+
+// paint pixels
+function paintElements(elementsColor) {
+  for (let i = 0; i < elementsColor.length; i += 1) {
+    elementsColor[i].style.backgroundColor = arrayColors[i];
+  }
+}
+
+// remove and add class selected
+function removeAddClassSelected(palette) {
+  palette.addEventListener('click', (event) => {
+    document.querySelector('.selected').classList.remove('selected');
+    event.target.classList.add('selected');
+  });
+}
+
+// paint the pixel board with the color that have the selected class
+function paintPixelCall(pixelBoard) {
+  pixelBoard.addEventListener('click', (event) => {
+    const selectedColor = document.querySelector('.selected');
+    event.target.style.backgroundColor = selectedColor.style.backgroundColor;
+  });
+}
+
+// Gerar board
+function gerarBoard(generateBoard) {
   generateBoard.addEventListener('click', () => {
     const pixels = document.querySelectorAll('.pixel');
     const pixelBoard = document.getElementById('pixel-board');
@@ -43,16 +49,30 @@ window.onload = () => {
       pixelBoard.removeChild(pixels[i]);
     }
     let size = document.getElementById('board-size').value;
-    if (size < 5)
-      size = 5;
-    if (size > 50)
-      size = 50;
-    pixelBoard.style.height = size * 42 + 'px';
-    pixelBoard.style.width = size * 42 + 'px';
+    if (size < 5) { size = 5; }
+    if (size > 50) { size = 50; }
+
+    pixelBoard.style.height = `${size * 42}px`;
+    pixelBoard.style.width = `${size * 42}px`;
     for (let i = 0; i < size * size; i += 1) {
       const newPixel = document.createElement('div');
       newPixel.classList.add('pixel');
       pixelBoard.appendChild(newPixel);
     }
-  })
+  });
+}
+
+window.onload = () => {
+  randomColors();
+  const elementsColor = document.getElementsByClassName('color');
+  const palette = document.getElementById('color-palette');
+  const pixelBoard = document.getElementById('pixel-board');
+  const clearBoard = document.getElementById('clear-board');
+  const generateBoard = document.getElementById('generate-board');
+
+  paintElements(elementsColor);
+  removeAddClassSelected(palette);
+  pixelBoard.addEventListener('click', paintPixelCall(pixelBoard));
+  clearBoard.addEventListener('click', clearBoard2(clearBoard));
+  generateBoard.addEventListener('click', gerarBoard(generateBoard));
 };
