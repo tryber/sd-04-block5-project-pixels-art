b82fd653e5c242b71de65e976181fd0e29794fad
Pedro Pires <pedrotpoliveira@gmail.com>
2020-04-13 18:51:52 -0300

    Commit 6 - Finer mods

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
-window.onload = function () {
-  const colors = document.getElementsByClassName('color');
-  let selectedColor = 'black';
-  function checkColor() {
-    for (let i = 0; i < 4; i += 1) {
-      colors[i].classList.remove('selected');
-    }
-    this.classList.add('selected');
-    selectedColor = this.classList.item(1);
-  }
+const colors = document.getElementsByClassName('color');
+let selectedColor = 'black';
+function checkColor() {
   for (let i = 0; i < 4; i += 1) {
-    colors[i].addEventListener('click', checkColor);
+    colors[i].classList.remove('selected');
   }
+  this.classList.add('selected');
+  selectedColor = this.classList.item(1);
+}
+for (let i = 0; i < 4; i += 1) {
+  colors[i].addEventListener('click', checkColor);
+}
 
-  const pixels = document.getElementsByClassName('pixel');
-  function checkPixel() {
-    if (this.classList.length === 2) {
-      this.classList.remove(this.classList.item(1));
-      this.classList.add(selectedColor);
-    } else {
-      this.classList.add(selectedColor);
-    }
+const pixels = document.getElementsByClassName('pixel');
+function checkPixel() {
+  if (this.classList.length === 2) {
+    this.classList.remove(this.classList.item(1));
+    this.classList.add(selectedColor);
+  } else {
+    this.classList.add(selectedColor);
   }
-  for (let i = 0; i < 25; i += 1) {
-    pixels[i].addEventListener('click', checkPixel);
-  }
-};
+}
+for (let i = 0; i < 25; i += 1) {
+  pixels[i].addEventListener('click', checkPixel);
+}
