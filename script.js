9a19840524d03e9515cd4110ea2cd32f0612d5ef
djonata-dias <diasdjonata@gmail.com>
2020-04-13 19:04:13 -0300

    adicionando cript de seleção de cor do palete

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 
 window.onload = function () {
+    let paletteCor1 = document.getElementById('cor1');
+    let paletteCor2 = document.getElementById('cor2');
+    let paletteCor3 = document.getElementById('cor3');
+    let paletteCor4 = document.getElementById('cor4');
+    let pixel = document.querySelector('div#pixel-board')
+    let colorSelected = 'black'
+    function selectColot (e) {
+        colorSelected = e.target.style.backgroundColor
+    }
+   document.onclick = console.log(e.target.style.backgroundColor);
     
 }
