b6d838ecd58e26e6050e471f8bbd85b6c1441934
Victor L <victor.lustosacp@gmail.com>
2020-04-13 17:49:38 -0300

    testando troca de cor dos pixels

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-let select = document.getElementById('color-palette');
-let color = 'black';
-select.addEventListener("click",function(event){
-    color = String(event.target.id);
-    console.log(color);
-})
-
-let table = document.getElementById('pixel-board');
-table.addEventListener("click",function(event){
-    event.target.id = color;
-})
+window.onload = function() {
+    let selected = document.getElementById('color-palette');
+    let color = 'black';
+    selected.addEventListener("click",function(event){
+        color = String(event.target.id);
+        console.log(color);
+    })
+    let table = document.getElementById('pixel-board');
+    table.addEventListener("click",function(event){
+        event.target.id = color;
+    })
+    
+}
 
 
 
