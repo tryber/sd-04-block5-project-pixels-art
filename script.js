727811771abdd5ed6101bd4d85b45c56a476a644
Julianafatsil <julianafatsil@gmail.com>
2020-04-14 19:27:34 -0300

    changes in the structure of HTML, change in the function CreatePixel, sum with index j, creation of the functionalities of picking up color and painting pixels

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
-function CreatePixel(){
+let matriz = document.getElementById('matriz');
+let pallet = document.getElementById('color-palette');
+let selectedColor = "black";
+
+function CreatePixel() {
     let linha;
     let coluna;
-    
-    for(let i = 0; i < 5; i++) {
+
+    for (let i = 0; i < 5; i++) {
         linha = document.createElement("div")
-        linha.id= "linha" + i;
+        linha.id = "linha" + i;
         document.getElementById("matriz").appendChild(linha);
-        for( let j = 0; j < 5; j++) {
+        for (let j = 0; j < 5; j++) {
             coluna = document.createElement("div");
-            coluna.id= "coluna" + j
+            coluna.id = "coluna" + i + j // alteracao linha + coluna
             coluna.classList.add("pixel");
             document.getElementById(linha.id).appendChild(coluna);
-         }
-         let divClear = document.createElement("div");
-         divClear.classList.add("clear");
-         document.getElementById(linha.id).appendChild(divClear);
-     } 
+        }
+        let divClear = document.createElement("div");
+        divClear.classList.add("clear");
+        document.getElementById(linha.id).appendChild(divClear);
+    }
 }
 
-console.log(CreatePixel());
+window.onload = function () {
+    CreatePixel();
+
+    pallet.addEventListener('click', function (event) {
+        selectedColor = event.target.id;
+    });
+
+    matriz.addEventListener('click', function (event) {
+        document.getElementById(event.target.id).classList = "pixel " + selectedColor;
+    })
+
+}
