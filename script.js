d670cc662da3d85fe03eda00baf348f248e5c39d
Pedro Pires <pedrotpoliveira@gmail.com>
2020-04-14 14:52:32 -0300

    Commit 7 - Reducing complexity

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ window.onload = function () {
   let selectedColor = 'black';
 
   function checkColor() {
-    for (let i = 0; i < 4; i += 1) {
-      colors[i].classList.remove('selected');
-    }
+    [...colors].forEach((element) => {
+      element.classList.remove('selected');
+    });
     this.classList.add('selected');
     selectedColor = this.classList.item(1);
   }
@@ -14,16 +14,14 @@ window.onload = function () {
   function checkPixel() {
     if (this.classList.length === 2) {
       this.classList.remove(this.classList.item(1));
-      this.classList.add(selectedColor);
-    } else {
-      this.classList.add(selectedColor);
     }
+    this.classList.add(selectedColor);
   }
 
-  for (let i = 0; i < 4; i += 1) {
-    colors[i].addEventListener('click', checkColor);
-  }
-  for (let i = 0; i < 25; i += 1) {
-    pixels[i].addEventListener('click', checkPixel);
-  }
+  [...colors].forEach((element) => {
+    element.addEventListener('click', checkColor);
+  });
+  [...pixels].forEach((element) => {
+    element.addEventListener('click', checkPixel);
+  });
 };
