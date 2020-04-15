b5dafd80761a818a55d271b7bc606991034284d6
djonata-dias <diasdjonata@gmail.com>
2020-04-14 21:31:25 -0300

    a paleta de cores está colorindo o quadro de  pixels

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
+let paletteColor = document.getElementsByClassName("color");
+let pixel = document.getElementsByClassName("pixel");
 
-window.onload = function () {
-    let paletteCor1 = document.getElementById('cor1');
-    let paletteCor2 = document.getElementById('cor2');
-    let paletteCor3 = document.getElementById('cor3');
-    let paletteCor4 = document.getElementById('cor4');
-    let pixel = document.querySelector('div#pixel-board')
-    let colorSelected = 'black'
-    function selectColot (e) {
-        colorSelected = e.target.style.backgroundColor
-    }
-   document.onclick = console.log(e.target.style.backgroundColor);
-    
+function getColor(evento) {
+  sessionStorage.color = evento.target.style.backgroundColor;
 }
+
+function paletaColor() {
+  for (let i = 0; i < paletteColor.length; i++) {
+    const red = Math.floor(Math.random() * 255);
+    const green = Math.floor(Math.random() * 255);
+    const blue = Math.floor(Math.random() * 255);
+    paletteColor[0].style.backgroundColor = "black";
+    paletteColor[i].style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+    paletteColor[i].addEventListener("click", getColor);
+  }
+}
+
+function corPixels() {
+  var colorSelected = sessionStorage.getItem("color");
+  for (let i = 0; i < pixel.length; i++) {
+    pixel[i].addEventListener("click", function (e) {
+      e.target.style.backgroundColor = sessionStorage.color;
+    });
+  }
+}
+
+paletaColor();
+corPixels();
