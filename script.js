a6c907b8f172fc00392d88ca2f662ca04daa3f7b
wistyk <leonardoxw@gmail.com>
2020-04-15 12:22:46 -0300

    updated

--- script.js
+++ script.js
@@ -3,12 +3,47 @@ function selecionar() {
   preto.classList.add('selected');
 }
 
-const cor = document.querySelector('.selected');
+let preto = document.querySelector('.preto');
+let verde = document.querySelector('.verde');
+let laranja = document.querySelector('.laranja');
+let amarelo = document.querySelector('.amarelo');
 
-const pixels = document.getElementsByClassName('pixel');
+preto.addEventListener('click', function () {
+  verde.classList.remove('selected');
+  laranja.classList.remove('selected');
+  amarelo.classList.remove('selected');
+  preto.classList.add('selected');
+  document.querySelector('.selected').style.backgroundColor = 'black';
+});
 
-for (let i = 0; i < pixels.length; i+= 1) {
+verde.addEventListener('click', function () {
+  preto.classList.remove('selected');
+  laranja.classList.remove('selected');
+  amarelo.classList.remove('selected');
+  verde.classList.add('selected');
+  document.querySelector('.selected').style.backgroundColor = 'green';
+});
+
+laranja.addEventListener('click', function () {
+  preto.classList.remove('selected');
+  verde.classList.remove('selected');
+  amarelo.classList.remove('selected');
+  laranja.classList.add('selected');
+  document.querySelector('.selected').style.backgroundColor = 'orange';
+});
+
+amarelo.addEventListener('click', function () {
+  preto.classList.remove('selected');
+  verde.classList.remove('selected');
+  laranja.classList.remove('selected');
+  amarelo.classList.add('selected');
+  document.querySelector('.selected').style.backgroundColor = 'yellow';
+});
+
+let pixels = document.getElementsByClassName('pixel');
+
+for (let i = 0; i < pixels.length; i += 1) {
   pixels[i].addEventListener('click', function () {
-    pixels[i].classList.add('selected');
+    pixels[i].classList.toggle('selected');
   });
 }
