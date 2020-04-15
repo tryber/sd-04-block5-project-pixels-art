8d585b00d61c265f553217fbe6703df15ea1cea3
wistyk <leonardoxw@gmail.com>
2020-04-14 21:18:47 -0300

    selected adicionado à cor preta por onload

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
+function selecionar() {
+  const preto = document.querySelector('.preto');
+  preto.classList.add('selected');
+}
+
 const cor = document.querySelector('.selected');
 
 const pixels = document.getElementsByClassName('pixel');
 
-for (let i = 0; i < pixels.length; i++) {
+for (let i = 0; i < pixels.length; i+= 1) {
   pixels[i].addEventListener('click', function () {
     pixels[i].classList.add('selected');
-    });
+  });
 }
