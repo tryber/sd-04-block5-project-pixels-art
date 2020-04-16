22b20c7d728ec51afc3267f26f2b4f285bdd75a4
Pedro Pires <pedrotpoliveira@gmail.com>
2020-04-15 22:59:08 -0300

    Commit Bonus Req 1 - 1st Attempt

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
-window.onload = function () {
-  const colors = document.getElementsByClassName('color');
-  const pixels = document.getElementsByClassName('pixel');
-  let selectedColor = 'black';
+const colors = document.getElementsByClassName('color');
+const pixels = document.getElementsByClassName('pixel');
+const btnClear = document.getElementById('clear-board');
+let selectedColor = 'black';
 
-  function checkColor() {
-    [...colors].forEach((element) => {
-      element.classList.remove('selected');
-    });
-    this.classList.add('selected');
-    selectedColor = this.classList.item(1);
-  }
-
-  function checkPixel() {
-    if (this.classList.length === 2) {
-      this.classList.remove(this.classList.item(1));
-    }
-    this.classList.add(selectedColor);
-  }
-
+function checkColor() {
   [...colors].forEach((element) => {
-    element.addEventListener('click', checkColor);
+    element.classList.remove('selected');
   });
+  this.classList.add('selected');
+  selectedColor = this.classList.item(1);
+}
+
+function checkPixel() {
+  if (this.classList.length === 2) {
+    this.classList.remove(this.classList.item(1));
+  }
+  this.classList.add(selectedColor);
+}
+
+[...colors].forEach((element) => {
+  element.addEventListener('click', checkColor);
+});
+[...pixels].forEach((element) => {
+  element.addEventListener('click', checkPixel);
+});
+btnClear.addEventListener('click', () => {
   [...pixels].forEach((element) => {
-    element.addEventListener('click', checkPixel);
+    element.classList.remove(element.classList.item(1));
   });
-};
+});
