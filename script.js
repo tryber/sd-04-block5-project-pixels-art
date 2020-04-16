004c2349445c23228575709b368462330cc18579
Pedro Pires <pedrotpoliveira@gmail.com>
2020-04-16 16:00:06 -0300

    Commit Bonus Req 2 - 3rd Attempt

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const colors = document.getElementsByClassName('color');
 const pixels = document.getElementsByClassName('pixel');
 const btnClear = document.getElementById('clear-board');
-const number = document.getElementById('board-size');
-const table = document.getElementById('board');
-const btnBoard = document.getElementById('generate-board');
+const boardSize = document.getElementById('board-size');
+const boardTable = document.getElementById('board');
+const btnGenBoard = document.getElementById('generate-board');
+
 let selectedColor = 'black';
 
 function checkColor() {
@@ -18,10 +19,42 @@ function checkPixel() {
   this.style.backgroundColor = selectedColor;
 }
 
+function addBoard(n) {
+  for (let i = 0; i < n; i += 1) {
+    const newTr = document.createElement('tr');
+    newTr.id = 'row';
+    boardTable.appendChild(newTr);
+  }
+  const rows = document.querySelectorAll('#row');
+  [...rows].forEach((element) => {
+    for (let i = 0; i < n; i += 1) {
+      const newTd = document.createElement('td');
+      newTd.className = 'pixel';
+      newTd.addEventListener('click', checkColor);
+      element.appendChild(newTd);
+    }
+  });
+}
+
+function deleteBoard() {
+  while (boardTable.hasChildNodes()) {
+    boardTable.removeChild(boardTable.firstChild);
+  }
+}
+
+btnGenBoard.addEventListener('click', () => {
+  deleteBoard();
+  addBoard(Number(boardSize.value));
+});
+
 [...colors].forEach((element) => {
   element.addEventListener('click', checkColor);
   element.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
   colors[0].style.backgroundColor = 'black';
+});
+
+[...pixels].forEach((element) => {
+  element.addEventListener('click', checkPixel);
 });
 
 btnClear.addEventListener('click', () => {
@@ -29,22 +62,3 @@ btnClear.addEventListener('click', () => {
     element.style.backgroundColor = 'white';
   });
 });
-
-function generateBoard() {
-  for (let i = 0; i < Number(number.value); i += 1) {
-    const tr = document.createElement('tr');
-    table.appendChild(tr);
-  }
-  const tr = document.getElementsByTagName('tr');
-  [...tr].forEach((element) => {
-    for (let i = 0; i < Number(number.value); i += 1) {
-      const td = document.createElement('td');
-      td.className = 'pixel';
-      td.addEventListener('click', checkPixel);
-      element.appendChild(td);
-    }
-  });
-}
-
-
-btnBoard.addEventListener('click', generateBoard);
