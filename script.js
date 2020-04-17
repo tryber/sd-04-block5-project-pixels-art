8aaa0ba0f85b8dbc90a1431a848d52d2a13de83c
Pedro Pires <pedrotpoliveira@gmail.com>
2020-04-17 16:38:20 -0300

    Bonus Req 2 - 5th Attempt

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 // Initialize vars
-const colors = document.getElementsByClassName('color');
-const pixels = document.getElementsByClassName('pixel');
-const rows = document.getElementsByClassName('row');
-const btnClear = document.getElementById('clear-board');
-const boardSize = document.getElementById('board-size');
-const boardTable = document.getElementById('board');
-const btnGenBoard = document.getElementById('generate-board');
-let selectedColor = 'black';
+const colors = document.getElementsByClassName("color");
+const pixels = document.getElementsByClassName("pixel");
+const rows = document.getElementsByClassName("row");
+const btnClear = document.getElementById("clear-board");
+const boardSize = document.getElementById("board-size");
+const boardTable = document.getElementById("board");
+const btnGenBoard = document.getElementById("generate-board");
+let selectedColor = "black";
 // func: Check which color is selected from palette
 function checkColor() {
   [...colors].forEach((element) => {
-    element.classList.remove('selected');
+    element.classList.remove("selected");
   });
-  this.classList.add('selected');
+  this.classList.add("selected");
   selectedColor = this.style.backgroundColor;
 }
 // func: Apply selected color from palette to pixel
@@ -29,7 +29,7 @@ function deleteRows(n) {
 function addRows(n) {
   for (let i = 0; i < n; i += 1) {
     const newTr = boardTable.insertRow();
-    newTr.className = 'row';
+    newTr.className = "row";
   }
 }
 // func: Add new cells (with eventlisteners) to board
@@ -37,33 +37,33 @@ function addCells(n) {
   [...rows].forEach((element) => {
     for (let i = 0; i < n; i += 1) {
       const newTd = element.insertCell();
-      newTd.className = 'pixel';
-      newTd.backgroundColor = 'white';
-      newTd.addEventListener('click', checkPixel);
+      newTd.className = "pixel";
+      newTd.backgroundColor = "white";
+      newTd.addEventListener("click", checkPixel);
     }
   });
 }
 // Initialize SPA
 window.onload = () => {
-  btnGenBoard.addEventListener('click', () => {
+  btnGenBoard.addEventListener("click", () => {
     deleteRows(rows.length);
     addRows(Number(boardSize.value));
     addCells(Number(boardSize.value));
   });
 
   [...colors].forEach((element) => {
-    element.addEventListener('click', checkColor);
+    element.addEventListener("click", checkColor);
     element.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-    colors[0].style.backgroundColor = 'black';
+    colors[0].style.backgroundColor = "black";
   });
 
   [...pixels].forEach((element) => {
-    element.addEventListener('click', checkPixel);
+    element.addEventListener("click", checkPixel);
   });
 
-  btnClear.addEventListener('click', () => {
+  btnClear.addEventListener("click", () => {
     [...pixels].forEach((element) => {
-      element.style.backgroundColor = 'white';
+      element.style.backgroundColor = "white";
     });
   });
 };
