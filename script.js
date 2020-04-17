2e3bae3fcf19c16d151229e9daf51d3584ebf587
Pedro Pires <pedrotpoliveira@gmail.com>
2020-04-17 15:24:57 -0300

    Bonus Req 2 - 4th Attempt

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
+// Initialize vars
 const colors = document.getElementsByClassName('color');
 const pixels = document.getElementsByClassName('pixel');
+const rows = document.getElementsByClassName('row');
 const btnClear = document.getElementById('clear-board');
 const boardSize = document.getElementById('board-size');
 const boardTable = document.getElementById('board');
 const btnGenBoard = document.getElementById('generate-board');
-
 let selectedColor = 'black';
-
+// func: Check which color is selected from palette
 function checkColor() {
   [...colors].forEach((element) => {
     element.classList.remove('selected');
@@ -14,51 +15,55 @@ function checkColor() {
   this.classList.add('selected');
   selectedColor = this.style.backgroundColor;
 }
-
+// func: Apply selected color from palette to pixel
 function checkPixel() {
   this.style.backgroundColor = selectedColor;
 }
-
-function addBoard(n) {
+// func: Delete all rows from board
+function deleteRows(n) {
   for (let i = 0; i < n; i += 1) {
-    const newTr = document.createElement('tr');
-    newTr.id = 'row';
-    boardTable.appendChild(newTr);
+    boardTable.deleteRow(0);
   }
-  const rows = document.querySelectorAll('#row');
+}
+// func: Add new rows to board
+function addRows(n) {
+  for (let i = 0; i < n; i += 1) {
+    const newTr = boardTable.insertRow();
+    newTr.className = 'row';
+  }
+}
+// func: Add new cells (with eventlisteners) to board
+function addCells(n) {
   [...rows].forEach((element) => {
     for (let i = 0; i < n; i += 1) {
-      const newTd = document.createElement('td');
+      const newTd = element.insertCell();
       newTd.className = 'pixel';
-      newTd.addEventListener('click', checkColor);
-      element.appendChild(newTd);
+      newTd.backgroundColor = 'white';
+      newTd.addEventListener('click', checkPixel);
     }
   });
 }
-
-function deleteBoard() {
-  while (boardTable.hasChildNodes()) {
-    boardTable.removeChild(boardTable.firstChild);
-  }
-}
-
-btnGenBoard.addEventListener('click', () => {
-  deleteBoard();
-  addBoard(Number(boardSize.value));
-});
-
-[...colors].forEach((element) => {
-  element.addEventListener('click', checkColor);
-  element.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-  colors[0].style.backgroundColor = 'black';
-});
-
-[...pixels].forEach((element) => {
-  element.addEventListener('click', checkPixel);
-});
-
-btnClear.addEventListener('click', () => {
-  [...pixels].forEach((element) => {
-    element.style.backgroundColor = 'white';
+// Initialize SPA
+window.onload = () => {
+  btnGenBoard.addEventListener('click', () => {
+    deleteRows(rows.length);
+    addRows(Number(boardSize.value));
+    addCells(Number(boardSize.value));
   });
-});
+
+  [...colors].forEach((element) => {
+    element.addEventListener('click', checkColor);
+    element.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    colors[0].style.backgroundColor = 'black';
+  });
+
+  [...pixels].forEach((element) => {
+    element.addEventListener('click', checkPixel);
+  });
+
+  btnClear.addEventListener('click', () => {
+    [...pixels].forEach((element) => {
+      element.style.backgroundColor = 'white';
+    });
+  });
+};
