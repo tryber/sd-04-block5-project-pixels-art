35e8d5d50dac36fc1bfd9110ac29ae7b2ccc70e4
yoshidalucas <lucaslimayoshida@gmail.com>
2020-04-17 18:05:43 -0300

    add random colors to palette on window load

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 window.onload = function () {
   const preto = this.document.getElementById('preto');
-  const vermelho = this.document.getElementById('vermelho');
-  const verde = this.document.getElementById('verde');
-  const azul = this.document.getElementById('azul');
-  preto.style.backgroundColor = 'black';
-  vermelho.style.backgroundColor = 'red';
-  verde.style.backgroundColor = 'green';
-  azul.style.backgroundColor = 'blue';
+  const dois = this.document.getElementById('dois');
+  const tres = this.document.getElementById('tres');
+  const quatro = this.document.getElementById('quatro');
   preto.className = 'selected color';
-  document.getElementById('preto').addEventListener('click', function () { preto.className = 'selected color'; vermelho.className = 'color'; verde.className = 'color'; azul.className = 'color'; });
-  document.getElementById('vermelho').addEventListener('click', function () { preto.className = 'color'; vermelho.className = 'selected color'; verde.className = 'color'; azul.className = 'color'; });
-  document.getElementById('verde').addEventListener('click', function () { preto.className = 'color'; vermelho.className = 'color'; verde.className = 'selected color'; azul.className = 'color'; });
-  document.getElementById('azul').addEventListener('click', function () { preto.className = 'color'; vermelho.className = 'color'; verde.className = 'color'; azul.className = 'selected color'; });
+  function randomColor(min, max) {
+    return ((Math.random() * (max - min)) + min);
+  }
+  dois.style.backgroundColor = 'rgb(' + randomColor(0, 255) + ',' + randomColor(0, 255) + ',' + randomColor(0, 255) + ')';
+  tres.style.backgroundColor = 'rgb(' + randomColor(0, 255) + ',' + randomColor(0, 255) + ',' + randomColor(0, 255) + ')';
+  quatro.style.backgroundColor = 'rgb(' + randomColor(0, 255) + ',' + randomColor(0, 255) + ',' + randomColor(0, 255) + ')';
+  preto.style.backgroundColor = 'black';
+  document.getElementById('preto').addEventListener('click', function () { preto.className = 'selected color'; dois.className = 'color'; tres.className = 'color'; quatro.className = 'color'; });
+  document.getElementById('dois').addEventListener('click', function () { preto.className = 'color'; dois.className = 'selected color'; tres.className = 'color'; quatro.className = 'color'; });
+  document.getElementById('tres').addEventListener('click', function () { preto.className = 'color'; dois.className = 'color'; tres.className = 'selected color'; quatro.className = 'color'; });
+  document.getElementById('quatro').addEventListener('click', function () { preto.className = 'color'; dois.className = 'color'; tres.className = 'color'; quatro.className = 'selected color'; });
   document.getElementById('pixel-board').addEventListener('click', function () { const selectedColor = document.querySelector('.selected'); event.target.style.backgroundColor = selectedColor.style.backgroundColor; });
 };
