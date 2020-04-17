4c462569c1cd1dcec505e145c481cc587816eb0e
yoshidalucas <lucaslimayoshida@gmail.com>
2020-04-16 23:42:52 -0300

    cor preta selecionada no carregamento gambi fodase

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 window.onload = function () {
-  document.getElementsByTagName('preto').className = '.selected';
+  //declaracao de variaveis para alterar cor da paleta
+  const preto = this.document.getElementById('preto');
+  const vermelho = this.document.getElementById('vermelho');
+  const verde = this.document.getElementById('verde');
+  const azul = this.document.getElementById('azul');
+  //adiciona cores a paleta
+  preto.style.backgroundColor= 'black';
+  vermelho.style.backgroundColor= 'red';
+  verde.style.backgroundColor= 'green';
+  azul.style.backgroundColor= 'blue';
+  // altera a classe da cor 1 para selecionada
+  preto.className = 'selected color';
 };
