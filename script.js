e98886217573dc37fdf38af2bea96c1a062c9607
Jorge Osiro <jojimurakabe@gmail.com>
2020-04-19 11:28:18 -0300

    fixing add em remove class

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 window.onload = function () {
   makeColor();
+  const black = document.querySelector('#black');
+  const red = document.querySelector('#red');
+  const green = document.querySelector('#green');
+  const blue = document.querySelector('#blue');
   const color = document.querySelectorAll('.color');
   const pixel = document.querySelectorAll('.pixel');
   const btnClr = document.querySelector('#clear-board');
@@ -13,36 +17,68 @@ window.onload = function () {
   for (let c = 0; c < 4; c += 1) {
     for (let i = 0; i < 25; i += 1) {
       color[c].addEventListener('click', function () {
-        theCSSprop = window.getComputedStyle(color[c], null).getPropertyValue('background-color');
-        pixel[i].addEventListener('click', function (e) {
-          this.style.backgroundColor = theCSSprop;
-        });
+        if (color[c].id == 'red') {
+          red.classList.add('selected');
+          black.classList.remove('selected');
+          green.classList.remove('selected');
+          blue.classList.remove('selected');
+          theCSSprop = window.getComputedStyle(color[c], null).getPropertyValue('background-color');
+          pixel[i].addEventListener('click', function (e) {
+            this.style.backgroundColor = theCSSprop;
+          });
+        } else if (color[c].id == 'black') {
+          red.classList.remove('selected');
+          black.classList.add('selected');
+          green.classList.remove('selected');
+          blue.classList.remove('selected');
+          theCSSprop = window.getComputedStyle(color[c], null).getPropertyValue('background-color');
+          pixel[i].addEventListener('click', function (e) {
+            this.style.backgroundColor = theCSSprop;
+          });
+        } else if (color[c].id == 'green') {
+          red.classList.remove('selected');
+          black.classList.remove('selected');
+          green.classList.add('selected');
+          blue.classList.remove('selected');
+          theCSSprop = window.getComputedStyle(color[c], null).getPropertyValue('background-color');
+          pixel[i].addEventListener('click', function (e) {
+            this.style.backgroundColor = theCSSprop;
+          });
+        } else if (color[c].id == 'blue') {
+          red.classList.remove('selected');
+          black.classList.remove('selected');
+          green.classList.remove('selected');
+          blue.classList.add('selected');
+          theCSSprop = window.getComputedStyle(color[c], null).getPropertyValue('background-color');
+          pixel[i].addEventListener('click', function (e) {
+            this.style.backgroundColor = theCSSprop;
+          });
+        }
       });
     }
   }
 
-  btnClr.addEventListener('click', function() {
+  btnClr.addEventListener('click', function () {
     mudaCor('white');
   });
-  let random = (Math.random() * (254-1) + 1)
-  console.log(Math.round(random));
 };
 
 function makeColor() {
-  let random = Math.round((Math.random() * (254-1) + 1));
+  let random1 = Math.round((Math.random() * (254 - 1) + 1));
+  let random2 = Math.round((Math.random() * (254 - 1) + 1));
   const black = document.querySelector('#black');
   black.style.backgroundColor = 'rgb(0, 0, 0)';
   const red = document.querySelector('#red');
-  red.style.backgroundColor = 'rgb(255,' + random + ', ' + random + ' )';
+  red.style.backgroundColor = 'rgb(255,' + random1 + ', ' + random2 + ' )';
   const green = document.querySelector('#green');
-  green.style.backgroundColor = 'rgb(' + random + ', 255, ' + random + ' )';
+  green.style.backgroundColor = 'rgb(' + random2 + ', 255, ' + random1 + ' )';
   const blue = document.querySelector('#blue');
-  blue.style.backgroundColor = 'rgb(' + random + ', ' + random + ', 255)';
+  blue.style.backgroundColor = 'rgb(' + random1 + ', ' + random2 + ', 255)';
 }
 
 function mudaCor(cor) {
   const pixel = document.querySelectorAll('.pixel');
-  for (let i = 0; i < 25; i+=1) {
+  for (let i = 0; i < 25; i += 1) {
     pixel[i].style.backgroundColor = cor;
   }
 }
