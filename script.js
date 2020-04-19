9f199317096058c8b654ca3e99f80e48e8ee8713
Jorge Osiro <jojimurakabe@gmail.com>
2020-04-19 11:46:18 -0300

    fixing code climate issues

--- script.js
+++ script.js
@@ -1,79 +1,19 @@
-window.onload = function () {
-  makeColor();
-  const black = document.querySelector('#black');
-  const red = document.querySelector('#red');
-  const green = document.querySelector('#green');
-  const blue = document.querySelector('#blue');
-  const color = document.querySelectorAll('.color');
-  const pixel = document.querySelectorAll('.pixel');
-  const btnClr = document.querySelector('#clear-board');
-  let theCSSprop;
-
-  for (let i = 0; i < 25; i += 1) {
-    pixel[i].addEventListener('click', function () {
-      pixel[i].style.backgroundColor = black.style.backgroundColor;
-    });
-  }
-  for (let c = 0; c < 4; c += 1) {
-    for (let i = 0; i < 25; i += 1) {
-      color[c].addEventListener('click', function () {
-        if (color[c].id == 'red') {
-          red.classList.add('selected');
-          black.classList.remove('selected');
-          green.classList.remove('selected');
-          blue.classList.remove('selected');
-          theCSSprop = window.getComputedStyle(color[c], null).getPropertyValue('background-color');
-          pixel[i].addEventListener('click', function (e) {
-            this.style.backgroundColor = theCSSprop;
-          });
-        } else if (color[c].id == 'black') {
-          red.classList.remove('selected');
-          black.classList.add('selected');
-          green.classList.remove('selected');
-          blue.classList.remove('selected');
-          theCSSprop = window.getComputedStyle(color[c], null).getPropertyValue('background-color');
-          pixel[i].addEventListener('click', function (e) {
-            this.style.backgroundColor = theCSSprop;
-          });
-        } else if (color[c].id == 'green') {
-          red.classList.remove('selected');
-          black.classList.remove('selected');
-          green.classList.add('selected');
-          blue.classList.remove('selected');
-          theCSSprop = window.getComputedStyle(color[c], null).getPropertyValue('background-color');
-          pixel[i].addEventListener('click', function (e) {
-            this.style.backgroundColor = theCSSprop;
-          });
-        } else if (color[c].id == 'blue') {
-          red.classList.remove('selected');
-          black.classList.remove('selected');
-          green.classList.remove('selected');
-          blue.classList.add('selected');
-          theCSSprop = window.getComputedStyle(color[c], null).getPropertyValue('background-color');
-          pixel[i].addEventListener('click', function (e) {
-            this.style.backgroundColor = theCSSprop;
-          });
-        }
-      });
-    }
-  }
-
-  btnClr.addEventListener('click', function () {
-    mudaCor('white');
-  });
-};
+const black = document.querySelector('#black');
+const red = document.querySelector('#red');
+const green = document.querySelector('#green');
+const blue = document.querySelector('#blue');
+const color = document.querySelectorAll('.color');
+const pixel = document.querySelectorAll('.pixel');
+const btnClr = document.querySelector('#clear-board');
 
 function makeColor() {
   let random1 = Math.round((Math.random() * (254 - 1) + 1));
   let random2 = Math.round((Math.random() * (254 - 1) + 1));
-  const black = document.querySelector('#black');
+
   black.style.backgroundColor = 'rgb(0, 0, 0)';
-  const red = document.querySelector('#red');
-  red.style.backgroundColor = 'rgb(255,' + random1 + ', ' + random2 + ' )';
-  const green = document.querySelector('#green');
-  green.style.backgroundColor = 'rgb(' + random2 + ', 255, ' + random1 + ' )';
-  const blue = document.querySelector('#blue');
-  blue.style.backgroundColor = 'rgb(' + random1 + ', ' + random2 + ', 255)';
+  red.style.backgroundColor = `rgb(255, ${random1}, ${random2} )`;
+  green.style.backgroundColor = `rgb( ${random2}, 255, ${random1} )`;
+  blue.style.backgroundColor = `rgb( ${random2}, ${random1}, 255 )`;
 }
 
 function mudaCor(cor) {
@@ -82,3 +22,64 @@ function mudaCor(cor) {
     pixel[i].style.backgroundColor = cor;
   }
 }
+
+function selectColor() {
+  let theCSSprop;
+  for (let c = 0; c < 4; c += 1) {
+    for (let i = 0; i < 25; i += 1) {
+      color[c].addEventListener('click', function () {
+        if (color[c].id ==='red') {
+          red.classList.add('selected');
+          black.classList.remove('selected');
+          green.classList.remove('selected');
+          blue.classList.remove('selected');
+          theCSSprop = window.getComputedStyle(color[c], null).getPropertyValue('background-color');
+          pixel[i].addEventListener('click', function () {
+            this.style.backgroundColor = theCSSprop;
+          });
+        } else if (color[c].id === 'black') {
+          red.classList.remove('selected');
+          black.classList.add('selected');
+          green.classList.remove('selected');
+          blue.classList.remove('selected');
+          theCSSprop = window.getComputedStyle(color[c], null).getPropertyValue('background-color');
+          pixel[i].addEventListener('click', function () {
+            this.style.backgroundColor = theCSSprop;
+          });
+        } else if (color[c].id === 'green') {
+          red.classList.remove('selected');
+          black.classList.remove('selected');
+          green.classList.add('selected');
+          blue.classList.remove('selected');
+          theCSSprop = window.getComputedStyle(color[c], null).getPropertyValue('background-color');
+          pixel[i].addEventListener('click', function () {
+            this.style.backgroundColor = theCSSprop;
+          });
+        } else if (color[c].id === 'blue') {
+          red.classList.remove('selected');
+          black.classList.remove('selected');
+          green.classList.remove('selected');
+          blue.classList.add('selected');
+          theCSSprop = window.getComputedStyle(color[c], null).getPropertyValue('background-color');
+          pixel[i].addEventListener('click', function () {
+            this.style.backgroundColor = theCSSprop;
+          });
+        }
+      });
+    }
+  }
+
+}
+
+window.onload = function () {
+  makeColor();
+  for (let i = 0; i < 25; i += 1) {
+    pixel[i].addEventListener('click', function () {
+      pixel[i].style.backgroundColor = black.style.backgroundColor;
+    });
+  }
+  selectColor()
+  btnClr.addEventListener('click', function () {
+    mudaCor('white');
+  });
+};
