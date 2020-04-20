40f45bd197c49955cf64eb0db4353f268f80de0b
lucasokazava <lucasokazava@gmail.com>
2020-04-20 19:03:19 -0300

    fix requisites 4 & 5 and add requisite 6

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 window.onload = function () {
-    const caixa1 = document.getElementsByClassName("color")[0];
-    const caixa2 = document.getElementsByClassName("color")[1];
-    const caixa3 = document.getElementsByClassName("color")[2];
-    const caixa4 = document.getElementsByClassName("color")[3];
+  const caixa1 = document.getElementsByClassName('color')[0];
+  const caixa2 = document.getElementsByClassName('color')[1];
+  const caixa3 = document.getElementsByClassName('color')[2];
+  const caixa4 = document.getElementsByClassName('color')[3];
 
-    caixa1.style.backgroundColor = "black";
-    caixa2.style.backgroundColor = "red";
-    caixa3.style.backgroundColor = "blue";
-    caixa4.style.backgroundColor = "green";
+  caixa1.style.backgroundColor = 'black';
+  caixa2.style.backgroundColor = 'red';
+  caixa3.style.backgroundColor = 'blue';
+  caixa4.style.backgroundColor = 'green';
 }
