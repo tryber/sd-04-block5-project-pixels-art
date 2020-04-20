c538a28acef3d5510692b59dbc4c715f805f70b7
yoshidalucas <lucaslimayoshida@gmail.com>
2020-04-20 14:45:59 -0300

    teste

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 window.onload = function () {
   const preto = this.document.getElementById('preto');
-  const dois = this.document.getElementById('dois').style.backgroundColor = `#${(Math.floor((Math.random() * (0, 16777215)) + 0)).toString(16)}`
-  const tres = this.document.getElementById('tres').style.backgroundColor = `#${(Math.floor((Math.random() * (0, 16777215)) + 0)).toString(16)}`;
-  const quatro = this.document.getElementById('quatro').style.backgroundColor = `#${(Math.floor((Math.random() * (0, 16777215)) + 0)).toString(16)}`;
+  const dois = this.document.getElementById('dois')
+  const tres = this.document.getElementById('tres');
+  const quatro = this.document.getElementById('quatro');
+  preto.className = 'selected color';
+  tres.style.backgroundColor = `#${(Math.floor((Math.random() * (0, 16777215)) + 0)).toString(16)}`;
+  ;dois.style.backgroundColor = `#${(Math.floor((Math.random() * (0, 16777215)) + 0)).toString(16)}`;
+  quatro.style.backgroundColor = `#${(Math.floor((Math.random() * (0, 16777215)) + 0)).toString(16)}`;
   document.getElementById('preto').addEventListener('click', function () { preto.className = 'selected color'; dois.className = 'color'; tres.className = 'color'; quatro.className = 'color'; });
   document.getElementById('dois').addEventListener('click', function () { preto.className = 'color'; dois.className = 'selected color'; tres.className = 'color'; quatro.className = 'color'; });
   document.getElementById('tres').addEventListener('click', function () { preto.className = 'color'; dois.className = 'color'; tres.className = 'selected color'; quatro.className = 'color'; });
