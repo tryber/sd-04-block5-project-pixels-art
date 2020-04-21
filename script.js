6d32151398ebd8be6f20da0adf8f1f002ff79818
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2020-04-21 10:13:15 -0300

    visando requisitos bonus

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-function createPixelBoard() {
-  let pixelBoard = document.getElementById("pixel-board");
+let pixelBoard = document.getElementById("pixel-board");
+function createPixelBoard(n) {
+  var qtd = 0
+  if(n<5){
+    qtd = 5;
+  } else if(n>50){
+    qtd = 50;
+  } else {qtd = n;}
   let finish = 0;
-  for(let i=0; i<5; i+=1){
-  if(finish<4){
-    for(let i=0; i<5; i+=1){
+  for(let i=0; i<qtd; i+=1){
+    let limite = qtd-1
+    if(finish<limite){
+    for(let i=0; i<qtd; i+=1){
       let pixel = document.createElement("div");
       pixel.setAttribute("class", "pixel");
       pixelBoard.appendChild(pixel);
@@ -12,7 +19,7 @@ function createPixelBoard() {
       pixelBoard.appendChild(br);
       finish += 1;
   } else {
-    for(let i=0; i<5; i+=1){
+    for(let i=0; i<qtd; i+=1){
       let pixel = document.createElement("div");
       pixel.setAttribute("class", "pixel")
       pixelBoard.appendChild(pixel);
@@ -32,9 +39,12 @@ function createPalette() {
   let childs = colorPalette.childNodes;
   childs[1].style.backgroundColor = "black";
   childs[1].classList.add("selected");
-  childs[2].style.backgroundColor = "blue";
-  childs[3].style.backgroundColor = "yellow";
-  childs[4].style.backgroundColor = "green";
+  let color2 = Math.floor(Math.random()*16777215).toString(16);
+  let color3 = Math.floor(Math.random()*16777215).toString(16);
+  let color4 = Math.floor(Math.random()*16777215).toString(16);
+  childs[2].style.backgroundColor = "#" + color2;
+  childs[3].style.backgroundColor = "#" + color3;
+  childs[4].style.backgroundColor = "#" + color4;
 }
 
 function addEvtPixels(painter){
@@ -59,6 +69,26 @@ function select(evt, paletteCollection){
   evt.target.classList.add("selected");
 }
 
+function addEvtClearAllPixels(painter){
+  let clearButton = document.getElementById("clear-board");
+  var painter = painter;
+  clearButton.addEventListener("click", function(){ painter.clearAllPixels()});
+}
+
+function addEvtBoardSize(){
+  let generateBoard = document.getElementById("generate-board");
+  generateBoard.addEventListener("click", reBoard);
+}
+
+function reBoard(){
+  let boardSize = document.getElementById("board-size");
+    while (pixelBoard.firstChild) {
+      pixelBoard.removeChild(pixelBoard.firstChild);
+  }
+    createPixelBoard(boardSize.value);
+    addEvtPixels(marquinhos);
+}
+
 function createPainter(){
   let painter = {
     color: null,
@@ -70,16 +100,23 @@ function createPainter(){
     },
     paint(evt){
       evt.target.style.backgroundColor = this.color[0].style.backgroundColor;
+    },
+    clearAllPixels(){
+      let pixelCollection = document.getElementsByClassName("pixel");
+      for(let i = 0; i < pixelCollection.length; i += 1)
+      pixelCollection[i].style.backgroundColor = "white";
     }
   }
   return painter;
 }
 
 
+
 createPalette();
 addEvtPalette();
-createPixelBoard();
+createPixelBoard(5);
 let marquinhos = new createPainter();
 marquinhos.selectedColor();
 addEvtPixels(marquinhos);
-
+addEvtClearAllPixels(marquinhos);
+addEvtBoardSize();
