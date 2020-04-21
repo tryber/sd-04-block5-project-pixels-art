0645392cbc5944129f2f658072a7bb8b4cb55a0e
lucasokazava <lucasokazava@gmail.com>
2020-04-21 11:36:54 -0300

    requisites completed + changes layout

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 window.onload = function () {
-  let caixa1 = document.getElementsByClassName('color')[0];
-  let caixa2 = document.getElementsByClassName('color')[1];
-  let caixa3 = document.getElementsByClassName('color')[2];
-  let caixa4 = document.getElementsByClassName('color')[3];
-  let pixelBoard = document.getElementById('pixel-board');
+  const color1 = document.getElementsByClassName('color')[0];
+  const color2 = document.getElementsByClassName('color')[1];
+  const color3 = document.getElementsByClassName('color')[2];
+  const color4 = document.getElementsByClassName('color')[3];
+  const pixelBoard = document.getElementById('pixel-board');
 
-  caixa1.style.backgroundColor = 'black';
-  caixa2.style.backgroundColor = 'red';
-  caixa3.style.backgroundColor = 'blue';
-  caixa4.style.backgroundColor = 'green';
-  caixa1.classList.add('selected');
+  color1.style.backgroundColor = 'black';
+  color2.style.backgroundColor = 'red';
+  color3.style.backgroundColor = 'blue';
+  color4.style.backgroundColor = 'green';
+  color1.classList.add('selected');
 
-  function inserindoCor (event) {
+  function selectColor(event) {
+    document.querySelector('.selected').classList.remove('selected');
+    event.target.classList.add('selected');
+  }
+  function insertColor(event) {
     event.target.style.backgroundColor = document.querySelector('.selected').style.backgroundColor;
   }
-
-  pixelBoard.addEventListener('click', inserindoCor);
+  color1.addEventListener('click', selectColor);
+  color2.addEventListener('click', selectColor);
+  color3.addEventListener('click', selectColor);
+  color4.addEventListener('click', selectColor);
+  pixelBoard.addEventListener('click', insertColor);
 }
