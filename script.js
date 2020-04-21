dcbe0b83414515ddb710a410135168f6091bc60b
Julianafatsil <julianafatsil@gmail.com>
2020-04-20 21:38:12 -0300

    Creating the Clear board button

--- script.js
+++ script.js
@@ -1,31 +1,39 @@
-const matriz = document.getElementById('pixel-board');
-const pallet = document.getElementById('color-palette');
+const MATRIZ = document.getElementById('pixel-board');
+const PALLET = document.getElementById('color-palette');
 let selectedColor = 'black';
+const CLEARBORD = document.getElementById('clear-board');
 
 function CreatePixel() {
   let linha;
   let coluna;
 
-  for (let i = 0; i < 5; i = i + 1) {
+  for (let i = 0; i < 5; i += 1) {
     linha = document.createElement('div');
     linha.id = `linha${i}`;
-    matriz.appendChild(linha);
-    for (let j = 0; j < 5; j = j + 1) {
+    MATRIZ.appendChild(linha);
+    for (let j = 0; j < 5; j += 1) {
       coluna = document.createElement('div');
       coluna.id = `coluna${i}${j}`;
       coluna.classList.add('pixel');
       document.getElementById(linha.id).appendChild(coluna);
     }
-    let divClear = document.createElement('div');
-    divClear.classList.add('clear');
-    document.getElementById(linha.id).appendChild(divClear);
+    const DIVCLEAR = document.createElement('div');
+    DIVCLEAR.classList.add('clear');
+    document.getElementById(linha.id).appendChild(DIVCLEAR);
+  }
+}
+
+function ClearBord() {
+  let pixel = document.getElementsByClassName('pixel');
+  for( let i = 0; i < pixel.length - 1; i += 1){
+    document.getElementById(pixel[i].id).classList = 'pixel';
   }
 }
 
 window.onload = function () {
   CreatePixel();
 
-  pallet.addEventListener('click', function (event) {
+  PALLET.addEventListener('click', function (event) {
     document.getElementById('black').classList.remove('selected');
     document.getElementById('red').classList.remove('selected');
     document.getElementById('blue').classList.remove('selected');
@@ -34,8 +42,11 @@ window.onload = function () {
     event.target.className += ' selected';
   });
 
-  matriz.addEventListener('click', function (event) {
+  MATRIZ.addEventListener('click', function (event) {
     document.getElementById(event.target.id).classList = `pixel ${selectedColor}`;
-  })
+  });
 
-}
+  CLEARBORD.addEventListener('click', function (event) {
+    ClearBord();
+  });
+};
