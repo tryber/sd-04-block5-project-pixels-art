76823b4141f8b29d06f62f02358dfb2296bccf48
lucasokazava <lucasokazava@gmail.com>
2020-04-21 01:14:07 -0300

    requisite 7

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 window.onload = function () {
-  const caixa1 = document.getElementsByClassName('color')[0];
-  const caixa2 = document.getElementsByClassName('color')[1];
-  const caixa3 = document.getElementsByClassName('color')[2];
-  const caixa4 = document.getElementsByClassName('color')[3];
+  let caixa1 = document.getElementsByClassName('color')[0];
+  let caixa2 = document.getElementsByClassName('color')[1];
+  let caixa3 = document.getElementsByClassName('color')[2];
+  let caixa4 = document.getElementsByClassName('color')[3];
+  let pixelBoard = document.getElementById('pixel-board');
 
+  caixa1.className = 'selected';
   caixa1.style.backgroundColor = 'black';
   caixa2.style.backgroundColor = 'red';
   caixa3.style.backgroundColor = 'blue';
   caixa4.style.backgroundColor = 'green';
+
+  function inserindoCor (event) {
+    event.target.style.backgroundColor = document.querySelector('.selected').style.backgroundColor;
+  }
+
+  pixelBoard.addEventListener('click', inserindoCor);
 }
